3ef51428b4bb8b28964f3d05636311a01e78cd9e
flabbet <flubbet@gmail.com>
2019-04-09 13:43:12 +0200

    Saving on disk feature backup

---
+++ PixiEditorDotNetCore3/Models/Tuple.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PixiEditorDotNetCore3.Models
+{
+    public class Tuple<T1, T2, T3> : IEquatable<Object>
+    {
+        public T1 Item1
+        {
+            get;
+            set;
+        }
+
+        public T2 Item2
+        {
+            get;
+            set;
+        }
+
+        public T3 Item3
+        {
+            get;
+            set;
+        }
+
+        public Tuple(T1 Item1, T2 Item2, T3 Item3)
+        {
+            this.Item1 = Item1;
+            this.Item2 = Item2;
+            this.Item3 = Item3;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || (obj as Tuple<T1, T2, T3>) == null) //if the object is null or the cast fails
+                return false;
+            else
+            {
+                Tuple<T1, T2, T3> tuple = (Tuple<T1, T2, T3>)obj;
+                return Item1.Equals(tuple.Item1) && Item2.Equals(tuple.Item2) && Item3.Equals(tuple.Item3);
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return Item1.GetHashCode() ^ Item2.GetHashCode() ^ Item3.GetHashCode();
+        }
+
+        public static bool operator ==(Tuple<T1, T2, T3> tuple1, Tuple<T1, T2, T3> tuple2)
+        {
+            return tuple1.Equals(tuple2);
+        }
+
+        public static bool operator !=(Tuple<T1, T2, T3> tuple1, Tuple<T1, T2, T3> tuple2)
+        {
+            return !tuple1.Equals(tuple2);
+        }
+    }
+}
---
+++ PixiEditorDotNetCore3/Models/UndoManager.cs
@@ -0,0 +1,206 @@
+﻿using PixiEditor.Helpers;
+using PixiEditorDotNetCore3.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing.Printing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace PixiEditor.Models
+{
+    public static class UndoManager
+    {
+        private const int MaximumChangesInRam = 1;
+
+        private static int _undosSavedOnDisk = 0;
+        private static int _redosSavedOnDisk = 0;
+        public static StackEx<Change> UndoStack { get; set; } = new StackEx<Change>(); 
+        public static StackEx<Change> RedoStack { get; set; } = new StackEx<Change>();
+        private static bool _stopRecording = false; 
+        private static List<Change> _recordedChanges = new List<Change>();
+        private static bool _lastChangeWasUndo = false;
+        public static bool CanUndo
+        {
+            get
+            {
+                return UndoStack.Count > 0;
+            }
+        }
+        public static bool CanRedo
+        {
+            get
+            {
+                return RedoStack.Count > 0;
+            }
+        }
+
+        public static object MainRoot { get; set; }
+
+        /// <summary>
+        /// Sets object(root) in which undo properties are stored.
+        /// </summary>
+        /// <param name="root">Parent object.</param>
+        public static void SetMainRoot(object root)
+        {
+            MainRoot = root;
+        }
+
+        /// <summary>
+        /// Records changes, used to save multiple changes as one
+        /// </summary>
+        /// <param name="property">Record property name.</param>
+        /// <param name="oldValue">Old change value.</param>
+        /// <param name="newValue">New change value.</param>
+        /// <param name="undoDescription">Description of change.</param>
+        public static void RecordChanges(string property, object oldValue, string undoDescription = "")
+        {
+            if (_stopRecording == false)
+            {
+                if (_recordedChanges.Count < 2)
+                {
+                    _recordedChanges.Add(new Change(property, oldValue, undoDescription));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Stops recording changes and saves it as one.
+        /// </summary>
+        public static void StopRecording()
+        {
+                _stopRecording = true;
+            if (_recordedChanges.Count > 0)
+            {
+                Change changeToSave = _recordedChanges[0];
+                AddUndoChange(changeToSave.Property, changeToSave.OldValue, changeToSave.Description);
+                _recordedChanges.Clear();
+            }
+            _stopRecording = false;
+        }
+        /// <summary>
+        /// Adds property change to UndoStack
+        /// </summary>
+        /// <param name="property">Changed property name.</param>
+        /// <param name="oldValue">Old value of property.</param>
+        /// <param name="newValue">New value of property.</param>
+        /// <param name="undoDescription">Description of change.</param>
+        public static void AddUndoChange(string property, object oldValue, string undoDescription = "")
+        {
+            if(_lastChangeWasUndo == false && RedoStack.Count > 0) //Cleares RedoStack if las move wasn't redo or undo and if redo stack is greater than 0
+            {
+                RedoStack.Clear();
+                PixiFilesManager.ClearDirectory(PixiFilesManager.RedoStackPath);
+            }
+            else if(_lastChangeWasUndo == true)
+            {
+                LoadChangeFromDiskIfAvailable(@"UndoStack\UndoChange", UndoStack, ref _undosSavedOnDisk);
+            }
+            else
+            {
+                WriteUndoChangeToDisk();
+            }
+            _lastChangeWasUndo = false;
+            UndoStack.Push(new Change(property, oldValue, undoDescription));
+            Debug.WriteLine("UndoStackCount: " + UndoStack.Count + " RedoStackCount: " + RedoStack.Count);
+        }
+
+        /// <summary>
+        /// Writes change to file if it is higher than some value (Saves only changes that contain serialized classes)
+        /// </summary>
+        private static void WriteUndoChangeToDisk()
+        {
+            if(UndoStack.Count > MaximumChangesInRam && _stopRecording == true)
+            {
+                _undosSavedOnDisk++;
+                PixiFilesManager.SaveObjectToJsonFile(new Change(UndoStack.First.Property, UndoStack.First.OldValue, UndoStack.First.Description), Path.Combine(PixiFilesManager.TempFolderPath, $@"UndoStack\UndoChange{_undosSavedOnDisk}.puc"));
+                UndoStack.Remove(0);
+            }
+        }
+
+        private static void WriteRedoChangeToDisk()
+        {
+            if (RedoStack.Count > MaximumChangesInRam)
+            {
+                PixiFilesManager.SaveObjectToJsonFile(new Change(RedoStack.First.Property, RedoStack.First.OldValue, RedoStack.First.Description), Path.Combine(PixiFilesManager.TempFolderPath, $@"RedoStack\RedoChange{_redosSavedOnDisk}.puc"));
+                RedoStack.Remove(0);
+                _redosSavedOnDisk++;
+            }
+        }
+
+        /// <summary>
+        /// Sets top property in UndoStack to Old Value
+        /// </summary>
+        public static void Undo()
+        { 
+            _lastChangeWasUndo = true;
+            PropertyInfo propInfo = MainRoot.GetType().GetProperty(UndoStack.Peek().Property);
+            propInfo.SetValue(MainRoot, UndoStack.Peek().OldValue);
+            RedoStack.Push(UndoStack.Pop());
+        }
+
+        /// <summary>
+        /// Sets top property from RedoStack to old value
+        /// </summary>
+        public static void Redo() 
+        {
+            _lastChangeWasUndo = true;
+            PropertyInfo propinfo = MainRoot.GetType().GetProperty(RedoStack.Peek().Property);
+            propinfo.SetValue(MainRoot, RedoStack.Pop().OldValue);
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="relativePath">Path to the folder inside Temp</param>
+        /// <param name="stack"></param>
+        /// <param name="changesSavedOnDisk"></param>
+        private static void LoadChangeFromDiskIfAvailable(string relativePath,StackEx<Change> stack, ref int changesSavedOnDisk)
+        {
+            if (changesSavedOnDisk > 0)
+            {
+                stack.PushToBottom(LoadChangeFromDisk(relativePath,ref changesSavedOnDisk));
+                PixiFilesManager.RemoveFile(Path.Combine(PixiFilesManager.TempFolderPath, $@"{relativePath}{changesSavedOnDisk+1}.puc"));
+            }
+        }
+
+        private static Change LoadChangeFromDisk(string relativePath, ref int changesSavedOnDisk)
+        {
+            Change result = PixiFilesManager.ReadObjectFromFile<Change>(Path.Combine(PixiFilesManager.TempFolderPath, $@"{relativePath}{changesSavedOnDisk}.puc"));
+            result.OldValue = TryFixOldValueDeserialization(result.OldValue);
+            changesSavedOnDisk--;
+            return result;
+        }
+
+        private static void FixUndoRecursionEffect()
+        {
+            PixiFilesManager.RemoveFile(Path.Combine(PixiFilesManager.UndoStackPath, $@"UndoStack/UndoChange{_undosSavedOnDisk}.puc"));
+            _undosSavedOnDisk--;
+        }
+
+
+        /// <summary>
+        /// Deserialzies JObject to LightLayer if possible
+        /// </summary>
+        /// <param name="changeToFix"></param>
+        /// <returns></returns>
+        private static object TryFixOldValueDeserialization(object oldValue)
+        {
+            try
+            {
+                return LightLayer.Deserialize(oldValue);
+            }
+            catch
+            {
+                return oldValue;
+            }
+        }
+    }
+}
