d3cdb11f26c19f569511c64b2b2c340b0092896f
flabbet <flubbet@gmail.com>
2020-08-14 21:02:05 +0200

    Updated version and install scripts

--- PixiEditor/Properties/AssemblyInfo.cs
+++ PixiEditor/Properties/AssemblyInfo.cs
@@ -50,5 +50,5 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.1.2.0")]
+[assembly: AssemblyFileVersion("0.1.2.0")]
