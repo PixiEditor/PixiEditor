6014642ad25f2b7a639ac63ddfcee820816d42cc
Polar <drewboyne@hotmail.com>
2020-09-12 22:45:06 +0100

    Remove Exit shortcut
    Remove the shortcut for the Exit (CloseWindow) Command.

--- PixiEditor/ViewModels/ViewModelMain.cs
+++ PixiEditor/ViewModels/ViewModelMain.cs
@@ -279,7 +279,6 @@ namespace PixiEditor.ViewModels
                     new Shortcut(Key.Delete, DeletePixelsCommand),
                     new Shortcut(Key.I, OpenResizePopupCommand, modifier: ModifierKeys.Control | ModifierKeys.Shift),
                     new Shortcut(Key.C, OpenResizePopupCommand, "canvas", ModifierKeys.Control | ModifierKeys.Shift),
-                    new Shortcut(Key.Q, SystemCommands.CloseWindowCommand, modifier: ModifierKeys.Control),
                     new Shortcut(Key.F11, SystemCommands.MaximizeWindowCommand),
                     //File
                     new Shortcut(Key.O, OpenFileCommand, modifier: ModifierKeys.Control),
