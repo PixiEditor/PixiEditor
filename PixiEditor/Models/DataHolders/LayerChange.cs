29c1d1b39cac10f7410e58c98622206eb42d1adf
Mikko Uuksulainen <mikko.uuksulainen@uuksu.fi>
2020-10-01 12:19:43 +0300

    Fix drawing getting slow with large canvas

--- PixiEditor/Models/DataHolders/LayerChange.cs
+++ PixiEditor/Models/DataHolders/LayerChange.cs
@@ -18,7 +18,16 @@ namespace PixiEditor.Models.DataHolders
         public LayerChange(BitmapPixelChanges pixelChanges, Layer layer)
         {
             PixelChanges = pixelChanges;
-            LayerIndex = ViewModelMain.Current.BitmapManager.ActiveDocument.Layers.IndexOf(layer);
+
+            // Layer implements IEquatable interface so IndexOf method cannot be used here as it
+            // calls Equals that is very slow (compares bitmap pixel by pixel).
+            for (var i = 0; i < ViewModelMain.Current.BitmapManager.ActiveDocument.Layers.Count; i++)
+            {
+                if (ViewModelMain.Current.BitmapManager.ActiveDocument.Layers[i] != layer) continue;
+
+                LayerIndex = i;
+                break;
+            }
         }
     }
 }
