c00e398946d6d1157259389d647098a9deec8fe4
ArtemK123 <darkmode1012@gmail.com>
2020-10-22 23:22:55 +0300

    Added Settings<T> in file to Setting and suppressed warning

--- PixiEditor/Models/Tools/ToolSettings/Settings/Setting.cs
+++ PixiEditor/Models/Tools/ToolSettings/Settings/Setting.cs
@@ -3,6 +3,30 @@ using PixiEditor.Helpers;
 
 namespace PixiEditor.Models.Tools.ToolSettings.Settings
 {
+    [System.Diagnostics.CodeAnalysis.SuppressMessage(
+        "StyleCop.CSharp.MaintainabilityRules",
+        "SA1402:File may only contain a single type",
+        Justification = "Same class with generic value")]
+    public abstract class Setting<T> : Setting
+    {
+        private T value;
+
+        protected Setting(string name)
+            : base(name)
+        {
+        }
+
+        public T Value
+        {
+            get => value;
+            set
+            {
+                this.value = value;
+                RaisePropertyChanged("Value");
+            }
+        }
+    }
+
     public abstract class Setting : NotifyableObject
     {
         protected Setting(string name)
