294477e08e17865904284f1a8f766dbae5b7f742
flabbet <flubbet@gmail.com>
2020-11-30 13:12:50 +0100

    Removed redundant type declaration

--- PixiEditor/ViewModels/SubViewModels/Main/UpdateViewModel.cs
+++ PixiEditor/ViewModels/SubViewModels/Main/UpdateViewModel.cs
@@ -86,7 +86,7 @@ namespace PixiEditor.ViewModels.SubViewModels.Main
 
         private async void Owner_OnStartupEvent(object sender, EventArgs e)
         {
-            if (PreferencesSettings.GetPreference<bool>("CheckUpdatesOnStartup", true))
+            if (PreferencesSettings.GetPreference("CheckUpdatesOnStartup", true))
             {
                 await CheckForUpdate();
             }
