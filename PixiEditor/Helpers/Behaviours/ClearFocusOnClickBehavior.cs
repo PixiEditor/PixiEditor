6d18d163b23c79f581aa68fb7de936c5697ea1b2
flabbet <flubbet@gmail.com>
2020-12-27 17:34:56 +0100

    Fixed focus bugs, now textboxes are updating on outside click

---
+++ PixiEditor/Helpers/Behaviours/ClearFocusOnClickBehavior.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Windows.Interactivity;
+
+namespace PixiEditor.Helpers.Behaviours
+{
+    public class ClearFocusOnClickBehavior : Behavior<FrameworkElement>
+    {
+        protected override void OnAttached()
+        {
+            AssociatedObject.MouseDown += AssociatedObject_MouseDown;
+            base.OnAttached();
+        }
+
+        private void AssociatedObject_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            AssociatedObject.Focus();
+        }
+
+        protected override void OnDetaching()
+        {
+            AssociatedObject.MouseDown -= AssociatedObject_MouseDown;
+        }
+    }
+}
