bc04ced3f2333468673350439c803ad0abcdd4dd
flabbet <flubbet@gmail.com>
2021-01-13 17:31:58 +0100

    WIP

---
+++ PixiEditor/Helpers/UI/UniversalTextBox.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows.Controls;
+using PixiEditor.Models.Controllers.Shortcuts;
+
+namespace PixiEditor.Helpers.UI
+{
+    public class UniversalTextBox : TextBox
+    {
+        public UniversalTextBox()
+            : base()
+        {
+            GotFocus += UniversalTextBox_GotFocus;
+            LostFocus += UniversalTextBox_LostFocus;
+        }
+
+        private void UniversalTextBox_GotFocus(object sender, System.Windows.RoutedEventArgs e)
+        {
+            ShortcutController.BlockShortcutExecution = true;
+        }
+
+        private void UniversalTextBox_LostFocus(object sender, System.Windows.RoutedEventArgs e)
+        {
+            ShortcutController.BlockShortcutExecution = false;
+        }
+    }
+}
