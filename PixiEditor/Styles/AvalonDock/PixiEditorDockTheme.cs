a92e30d6f3273c7953e02aa51c672626d639d9b5
flabbet <flubbet@gmail.com>
2021-01-23 17:32:16 +0100

    WIP

---
+++ PixiEditor/Styles/AvalonDock/PixiEditorDockTheme.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using AvalonDock.Themes;
+
+namespace PixiEditor.Styles.AvalonDock
+{
+    public class PixiEditorDockTheme : Theme
+    {
+        public override Uri GetResourceUri()
+        {
+            return new Uri("/PixiEditor;component/Styles/AvalonDock/PixiEditorDockTheme.xaml", UriKind.Relative);
+        }
+    }
+}
