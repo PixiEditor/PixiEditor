2f3451b4d41cd8e622c4899ed163e95b7f01dab4
flabbet <flubbet@gmail.com>
2021-01-24 20:59:53 +0100

    Added Styles/AvalonDock custom style (VS2013 fork)

--- PixiEditor/Styles/AvalonDock/PixiEditorDockTheme.cs
+++ PixiEditor/Styles/AvalonDock/PixiEditorDockTheme.cs
@@ -1,13 +1,26 @@
-﻿using System;
+﻿/************************************************************************
+   AvalonDock
+
+   Copyright (C) 2007-2013 Xceed Software Inc.
+
+   This program is provided to you under the terms of the Microsoft Public
+   License (Ms-PL) as published at https://opensource.org/licenses/MS-PL
+ ************************************************************************/
+
+using System;
 using AvalonDock.Themes;
 
 namespace PixiEditor.Styles.AvalonDock
 {
+    /// <inheritdoc/>
     public class PixiEditorDockTheme : Theme
     {
+        /// <inheritdoc/>
         public override Uri GetResourceUri()
         {
-            return new Uri("/PixiEditor;component/Styles/AvalonDock/PixiEditorDockTheme.xaml", UriKind.Relative);
+            return new Uri(
+                "/PixiEditor;component/Styles/AvalonDock/PixiEditorDockTheme.xaml",
+                UriKind.Relative);
         }
     }
 }
