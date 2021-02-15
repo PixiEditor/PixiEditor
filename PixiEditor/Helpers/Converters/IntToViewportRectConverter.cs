c6bad7ad2ee273620a6b19a9a81bc33a6ef4de26
flabbet <flubbet@gmail.com>
2021-02-15 13:14:46 +0100

    Lines WIP

---
+++ PixiEditor/Helpers/Converters/IntToViewportRectConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace PixiEditor.Helpers.Converters
+{
+    public class IntToViewportRectConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (parameter is string and "vertical")
+            {
+                return new Rect(0, 0, 1d / (int)value, 1d);
+            }
+
+            return new Rect(0, 0, 1d, 1d / (int)value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
