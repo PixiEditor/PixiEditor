8919feff0618306656f6753b5a3e8abe7deb54b8
CPKreuz <chrisphilkreuzgaming04@gmail.com>
2021-03-07 20:42:44 +0100

    Added SetProperty<T> to NotifyableObject

--- PixiEditor/NotifyableObject.cs
+++ PixiEditor/NotifyableObject.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace PixiEditor.Helpers
 {
@@ -16,5 +18,18 @@ namespace PixiEditor.Helpers
                 PropertyChanged(this, new PropertyChangedEventArgs(property));
             }
         }
+
+        protected bool SetProperty<T>(ref T backingStore, T value, [CallerMemberName] string propertyName = "")
+        {
+            if (EqualityComparer<T>.Default.Equals(backingStore, value))
+            {
+                return false;
+            }
+
+            backingStore = value;
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            return true;
+        }
+
     }
 }
