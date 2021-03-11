1a92bf932c034abf00cc8281ff5c28e19cb31f13
CPKreuz <chrisphilkreuzgaming04@gmail.com>
2021-03-11 19:11:18 +0100

    Removed break from debug menu

--- PixiEditor/ViewModels/SubViewModels/Main/DebugViewModel.cs
+++ PixiEditor/ViewModels/SubViewModels/Main/DebugViewModel.cs
@@ -5,19 +5,9 @@ namespace PixiEditor.ViewModels.SubViewModels.Main
 {
     public class DebugViewModel : SubViewModel<ViewModelMain>
     {
-        public RelayCommand BreakCommand { get; set; }
-
         public DebugViewModel(ViewModelMain owner)
             : base(owner)
         {
-            BreakCommand = new RelayCommand(Break, CanBreak);
         }
-
-        public static void Break(object parameter)
-        {
-            Debugger.Break();
-        }
-
-        public static bool CanBreak(object parameter) => Debugger.IsAttached;
     }
 }
