1be08991bc160369d38369c6eb558c83442b7498
flabbet <flubbet@gmail.com>
2021-07-03 13:39:45 +0200

    Visibility typos

--- PixiEditor/Helpers/Converters/EmptyStringToVisibiltyConverter.cs
+++ PixiEditor/Helpers/Converters/EmptyStringToVisibiltyConverter.cs
@@ -9,7 +9,7 @@ using System.Windows.Data;
 
 namespace PixiEditor.Helpers.Converters
 {
-    class EmptyStringToVisibiltyConverter : IValueConverter
+    class EmptyStringToVisibilityConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
