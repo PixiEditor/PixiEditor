6decc07d16c6d0ca6cac41afce60355ffde5a29d
CPKreuz <chrisphilkreuzgaming04@gmail.com>
2021-07-08 21:24:37 +0200

    Removed BitmapManager service from MoveViewportTool

--- PixiEditor/Models/Tools/Tools/MoveViewportTool.cs
+++ PixiEditor/Models/Tools/Tools/MoveViewportTool.cs
@@ -8,18 +8,15 @@ namespace PixiEditor.Models.Tools.Tools
 {
     public class MoveViewportTool : ReadonlyTool
     {
-        private BitmapManager BitmapManager { get; }
-
         private ToolsViewModel ToolsViewModel { get; }
 
-        public MoveViewportTool(BitmapManager bitmapManager, ToolsViewModel toolsViewModel)
+        public MoveViewportTool(ToolsViewModel toolsViewModel)
         {
             HideHighlight = true;
             Cursor = Cursors.SizeAll;
             ActionDisplay = "Click and move to pan viewport.";
             Tooltip = "Move viewport. (H)";
 
-            BitmapManager = bitmapManager;
             ToolsViewModel = toolsViewModel;
         }
 
