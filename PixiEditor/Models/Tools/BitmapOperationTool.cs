b197dc200bf982460f8d01216852d79c6a30120f
Rene Brokholm <rebizu@gmail.com>
2021-07-27 20:18:10 +0200

    Update PixiEditor/Models/Tools/BitmapOperationTool.cs
    Co-authored-by: CPKreuz <45312141+CPKreuz@users.noreply.github.com>

--- PixiEditor/Models/Tools/BitmapOperationTool.cs
+++ PixiEditor/Models/Tools/BitmapOperationTool.cs
@@ -15,7 +15,7 @@ namespace PixiEditor.Models.Tools
         public bool ClearPreviewLayerOnEachIteration { get; set; } = true;
 
         public bool UseDefaultUndoMethod { get; set; } = true;
-        public virtual bool UsesShift { get => true; }
+        public virtual bool UsesShift => true;
 
         private readonly LayerChange[] onlyLayerArr = new LayerChange[] { new LayerChange(BitmapPixelChanges.Empty, Guid.Empty) };
 
