b41a27529ed787c6eebc269297c8d5af705862df
Krzysztof Krysiński <flubbet@gmail.com>
2022-01-20 17:35:23 +0100

    Fixed paste undo and PNG unsupported color format pasting

--- PixiEditor/Models/Controllers/ClipboardController.cs
+++ PixiEditor/Models/Controllers/ClipboardController.cs
@@ -290,7 +290,7 @@ namespace PixiEditor.Models.Controllers
                     FormatConvertedBitmap newFormat = new FormatConvertedBitmap();
                     newFormat.BeginInit();
                     newFormat.Source = source;
-                    newFormat.DestinationFormat = PixelFormats.Rgba64;
+                    newFormat.DestinationFormat = PixelFormats.Bgra32;
                     newFormat.EndInit();
 
                     result = new Surface(newFormat);
@@ -315,7 +315,7 @@ namespace PixiEditor.Models.Controllers
 
             while (i < document.Layers.Count)
             {
-                document.RemoveLayer(i, true);
+                document.RemoveLayer(i, false);
             }
         }
 
