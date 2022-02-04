954c75af02ab52e031074558b5029dfbf019ca08
tomaszkot <tomkot@gmail.com>
2022-02-04 19:56:33 +0100

    Please don't use a 2 to replace the word To

--- PixiEditor/Helpers/Converters/FileExtensionToColorConverter.cs
+++ PixiEditor/Helpers/Converters/FileExtensionToColorConverter.cs
@@ -12,29 +12,29 @@ namespace PixiEditor.Helpers.Converters
     public class FileExtensionToColorConverter :
         SingleInstanceConverter<FileExtensionToColorConverter>
     {
-        private static readonly Dictionary<string, SolidColorBrush> extensions2Brushes;
+        private static readonly Dictionary<string, SolidColorBrush> extensionsToBrushes;
         public static readonly SolidColorBrush UnknownBrush = ColorBrush(100, 100, 100);
 
         static FileExtensionToColorConverter()
         {
-            extensions2Brushes = new Dictionary<string, SolidColorBrush>();
-            AssignFormat2Brush(FileType.Unset, UnknownBrush);
-            AssignFormat2Brush(FileType.Pixi, ColorBrush(226, 1, 45));
-            AssignFormat2Brush(FileType.Png, ColorBrush(56, 108, 254));
-            AssignFormat2Brush(FileType.Jpeg, ColorBrush(36, 179, 66));
-            AssignFormat2Brush(FileType.Bmp, ColorBrush(255, 140, 0));
-            AssignFormat2Brush(FileType.Gif, ColorBrush(180, 0, 255));
+            extensionsToBrushes = new Dictionary<string, SolidColorBrush>();
+            AssignFormatToBrush(FileType.Unset, UnknownBrush);
+            AssignFormatToBrush(FileType.Pixi, ColorBrush(226, 1, 45));
+            AssignFormatToBrush(FileType.Png, ColorBrush(56, 108, 254));
+            AssignFormatToBrush(FileType.Jpeg, ColorBrush(36, 179, 66));
+            AssignFormatToBrush(FileType.Bmp, ColorBrush(255, 140, 0));
+            AssignFormatToBrush(FileType.Gif, ColorBrush(180, 0, 255));
         }
-        static void AssignFormat2Brush(FileType format, SolidColorBrush brush)
+        static void AssignFormatToBrush(FileType format, SolidColorBrush brush)
         {
-            SupportedFilesHelper.GetFileTypeDialogData(format).Extensions.ForEach(i => extensions2Brushes[i] = brush);
+            SupportedFilesHelper.GetFileTypeDialogData(format).Extensions.ForEach(i => extensionsToBrushes[i] = brush);
         }
         
         public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             string extension = (string)value;
 
-            return extensions2Brushes.ContainsKey(extension) ? extensions2Brushes[extension] : UnknownBrush;
+            return extensionsToBrushes.ContainsKey(extension) ? extensionsToBrushes[extension] : UnknownBrush;
         }
 
         private static SolidColorBrush ColorBrush(byte r, byte g, byte b)
