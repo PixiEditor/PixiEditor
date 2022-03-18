475b7c2d62c6b514326e673e805f0ab4fca61e11
Krzysztof Krysiński <flubbet@gmail.com>
2022-03-18 19:45:08 +0100

    Palette context menu

--- PixiEditor/Views/UserControls/Palettes/PaletteColor.xaml.cs
+++ PixiEditor/Views/UserControls/Palettes/PaletteColor.xaml.cs
@@ -26,6 +26,17 @@ public partial class PaletteColor : UserControl
     public static readonly DependencyProperty AssociatedKeyProperty =
         DependencyProperty.Register("AssociatedKey", typeof(int?), typeof(PaletteColor), new PropertyMetadata(null));
 
+    public CornerRadius CornerRadius
+    {
+        get { return (CornerRadius)GetValue(CornerRadiusProperty); }
+        set { SetValue(CornerRadiusProperty, value); }
+    }
+
+    // Using a DependencyProperty as the backing store for CornerRadius.  This enables animation, styling, binding, etc...
+    public static readonly DependencyProperty CornerRadiusProperty =
+        DependencyProperty.Register("CornerRadius", typeof(CornerRadius), typeof(PaletteColor), new PropertyMetadata(new CornerRadius(5f)));
+
+
 
     public PaletteColor()
     {
