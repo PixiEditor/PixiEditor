7461b65ba4ddba7396798332da8748a5d7590ff5
flabbet <flubbet@gmail.com>
2022-03-21 21:14:15 +0100

    Added compact mode

---
+++ PixiEditor/Views/UserControls/Palettes/CompactPaletteViewer.xaml.cs
@@ -0,0 +1,48 @@
+﻿using PixiEditor.Models.DataHolders;
+using SkiaSharp;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace PixiEditor.Views.UserControls.Palettes
+{
+    /// <summary>
+    /// Interaction logic for CompactPaletteViewer.xaml
+    /// </summary>
+    public partial class CompactPaletteViewer : UserControl
+    {
+        public static readonly DependencyProperty ColorsProperty = DependencyProperty.Register(
+            "Colors", typeof(WpfObservableRangeCollection<SKColor>), typeof(CompactPaletteViewer));
+
+        public WpfObservableRangeCollection<SKColor> Colors
+        {
+            get { return (WpfObservableRangeCollection<SKColor>)GetValue(ColorsProperty); }
+            set { SetValue(ColorsProperty, value); }
+        }
+
+        public ICommand SelectColorCommand
+        {
+            get { return (ICommand)GetValue(SelectColorCommandProperty); }
+            set { SetValue(SelectColorCommandProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for SelectColorCommand.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty SelectColorCommandProperty =
+            DependencyProperty.Register("SelectColorCommand", typeof(ICommand), typeof(CompactPaletteViewer));
+
+        public CompactPaletteViewer()
+        {
+            InitializeComponent();
+        }
+
+        private void RemoveColorMenuItem_OnClick(object sender, RoutedEventArgs e)
+        {
+            MenuItem menuItem = (MenuItem)sender;
+            SKColor color = (SKColor)menuItem.CommandParameter;
+            if (Colors.Contains(color))
+            {
+                Colors.Remove(color);
+            }
+        }
+    }
+}
