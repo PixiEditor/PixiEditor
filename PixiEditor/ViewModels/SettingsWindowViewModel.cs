e35247082f4c0ae49690cafdd7ca550f09977d1d
CPKreuz <chrisphilkreuzgaming04@gmail.com>
2022-04-30 14:06:37 +0200

    Implemented search

--- PixiEditor/ViewModels/SettingsWindowViewModel.cs
+++ PixiEditor/ViewModels/SettingsWindowViewModel.cs
@@ -1,13 +1,17 @@
-﻿using PixiEditor.Models.Commands;
-using PixiEditor.Models.Commands.Attributes;
+﻿using PixiEditor.Helpers;
+using PixiEditor.Models.Commands;
+using PixiEditor.Models.DataHolders;
 using PixiEditor.Models.Dialogs;
 using PixiEditor.ViewModels.SubViewModels.UserPreferences;
-using System.Collections.ObjectModel;
+using System.Windows;
 
 namespace PixiEditor.ViewModels
 {
     public class SettingsWindowViewModel : ViewModelBase
     {
+        private string searchTerm;
+        private int visibleGroups;
+
         public bool ShowUpdateTab
         {
             get
@@ -20,9 +24,29 @@ namespace PixiEditor.ViewModels
             }
         }
 
+        public string SearchTerm
+        {
+            get => searchTerm;
+            set
+            {
+                if (SetProperty(ref searchTerm, value, out var oldValue) &&
+                    !(string.IsNullOrWhiteSpace(value) && string.IsNullOrWhiteSpace(oldValue)))
+                {
+                    UpdateSearchResults();
+                    VisibleGroups = Commands.Count(x => x.Visibility == Visibility.Visible);
+                }
+            }
+        }
+
+        public int VisibleGroups
+        {
+            get => visibleGroups;
+            private set => SetProperty(ref visibleGroups, value);
+        }
+
         public SettingsViewModel SettingsSubViewModel { get; set; }
 
-        public ObservableCollection<CommandGroup> Commands { get; }
+        public List<GroupSearchResult> Commands { get; }
 
         [Models.Commands.Attributes.Command.Internal("PixiEditor.Shortcuts.Reset")]
         public static void ResetCommand()
@@ -36,8 +60,82 @@ namespace PixiEditor.ViewModels
 
         public SettingsWindowViewModel()
         {
-            Commands = new(CommandController.Current.CommandGroups);
+            Commands = new(CommandController.Current.CommandGroups.Select(x => new GroupSearchResult(x)));
             SettingsSubViewModel = new SettingsViewModel(this);
+        }
+
+        public void UpdateSearchResults()
+        {
+            if (string.IsNullOrWhiteSpace(searchTerm))
+            {
+                foreach (var group in Commands)
+                {
+                    group.Visibility = Visibility.Visible;
+                    foreach (var command in group.Commands)
+                    {
+                        command.Visibility = Visibility.Visible;
+                    }
+                }
+                return;
+            }
+
+            foreach (var group in Commands)
+            {
+                int visibleCommands = 0;
+                foreach (var command in group.Commands)
+                {
+                    if (command.Command.Display.Contains(SearchTerm, StringComparison.OrdinalIgnoreCase))
+                    {
+                        visibleCommands++;
+                        command.Visibility = Visibility.Visible;
+                    }
+                    else
+                    {
+                        command.Visibility = Visibility.Collapsed;
+                    }
+                }
+
+                group.Visibility = visibleCommands > 0 ? Visibility.Visible : Visibility.Collapsed;
+            }
+        }
+
+        public class GroupSearchResult : NotifyableObject
+        {
+            private Visibility visibility;
+
+            public string Display { get; set; }
+
+            public List<CommandSearchResult> Commands { get; set; }
+
+            public Visibility Visibility
+            {
+                get => visibility;
+                set => SetProperty(ref visibility, value);
+            }
+
+            public GroupSearchResult(CommandGroup group)
+            {
+                Display = group.Display;
+                Commands = new(group.VisibleCommands.Select(x => new CommandSearchResult(x)));
+            }
+        }
+
+        public class CommandSearchResult : NotifyableObject
+        {
+            private Visibility visibility;
+
+            public Command Command { get; set; }
+
+            public Visibility Visibility
+            {
+                get => visibility;
+                set => SetProperty(ref visibility, value);
+            }
+
+            public CommandSearchResult(Command command)
+            {
+                Command = command;
+            }
         }
     }
 }
