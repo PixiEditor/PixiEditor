7e41d2f0ddfc0aca0196e8aa62e95e3e78b15cff
CPKreuz <chrisphilkreuzgaming04@gmail.com>
2022-06-18 03:29:10 +0200

    Added docs to Change.cs and UpdateableChange.cs

--- src/PixiEditor.ChangeableDocument/Changes/UpdateableChange.cs
+++ src/PixiEditor.ChangeableDocument/Changes/UpdateableChange.cs
@@ -2,5 +2,10 @@
 
 internal abstract class UpdateableChange : Change
 {
+    /// <summary>
+    /// Applies the change temporarily
+    /// </summary>
+    /// <param name="target">The document the change will be applied on</param>
+    /// <returns>Either nothing, a single change info, or a collection of change infos</returns>
     public abstract OneOf<None, IChangeInfo, List<IChangeInfo>> ApplyTemporarily(Document target);
 }
