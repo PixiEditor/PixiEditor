7762fcfdb36bd1e30a6365ff0a5e8420fed3a0e8
CPKreuz <chrisphilkreuzgaming04@gmail.com>
2022-10-28 19:21:26 +0200

    Fixed Aseprite Provider

--- src/PixiEditor/Models/Commands/Templates/Providers/AsepriteProvider.cs
+++ src/PixiEditor/Models/Commands/Templates/Providers/AsepriteProvider.cs
@@ -17,10 +17,10 @@ internal partial class ShortcutProvider
             { "PixiEditor.File.SaveAsNew", Key.S, ModifierKeys.Control | ModifierKeys.Alt },
             { "PixiEditor.Window.OpenSettingsWindow", Key.K, ModifierKeys.Control },
             // Tools
-            { "PixiEditor.Tools.Select.CircleTool", Key.U, ModifierKeys.Shift },
-            { "PixiEditor.Tools.Select.ColorPickerTool", Key.I, ModifierKeys.None },
-            { "PixiEditor.Tools.Select.RectangleTool", Key.U, ModifierKeys.None },
-            { "PixiEditor.Tools.Select.SelectTool", Key.V, ModifierKeys.None },
+            { "PixiEditor.Tools.Select.EllipseToolViewModel", Key.U, ModifierKeys.Shift },
+            { "PixiEditor.Tools.Select.ColorPickerToolViewModel", Key.I, ModifierKeys.None },
+            { "PixiEditor.Tools.Select.RectangleToolViewModel", Key.U, ModifierKeys.None },
+            { "PixiEditor.Tools.Select.SelectToolViewModel", Key.V, ModifierKeys.None },
             // Not actually in aseprite, but should be included
             { "PixiEditor.Search.Toggle", Key.OemComma, ModifierKeys.Control }
         };
