c040f727a38f3e7be2397769375b586891b131c5
Equbuxu <egor08281@gmail.com>
2023-04-07 22:18:06 +0300

    Specify version more precisely in crash reports

--- src/PixiEditor/Helpers/VersionHelpers.cs
+++ src/PixiEditor/Helpers/VersionHelpers.cs
@@ -10,21 +10,33 @@ internal static class VersionHelpers
 
     public static string GetCurrentAssemblyVersion(Func<Version, string> toString) => toString(GetCurrentAssemblyVersion());
 
-    public static string GetCurrentAssemblyVersionString()
+    public static string GetCurrentAssemblyVersionString(bool moreSpecific = false)
     {
         StringBuilder builder = new(GetCurrentAssemblyVersion().ToString());
 
         bool isDone = false;
 
         AppendDevBuild(builder, ref isDone);
-
         if (isDone)
-        {
             return builder.ToString();
-        }
+
+        AppendMsixDebugBuild(builder, ref isDone);
+        if (isDone)
+            return builder.ToString();
 
         AppendDebugBuild(builder, ref isDone);
+        if (isDone || !moreSpecific)
+            return builder.ToString();
 
+        AppendSteamBuild(builder, ref isDone);
+        if (isDone)
+            return builder.ToString();
+
+        AppendMsixBuild(builder, ref isDone);
+        if (isDone)
+            return builder.ToString();
+
+        AppendReleaseBuild(builder, ref isDone);
         return builder.ToString();
     }
 
@@ -42,5 +54,37 @@ internal static class VersionHelpers
         done = true;
 
         builder.Append(" Debug Build");
+    }
+
+    [Conditional("RELEASE")]
+    private static void AppendReleaseBuild(StringBuilder builder, ref bool done)
+    {
+        done = true;
+
+        builder.Append(" Release Build");
+    }
+
+    [Conditional("MSIX_DEBUG")]
+    private static void AppendMsixDebugBuild(StringBuilder builder, ref bool done)
+    {
+        done = true;
+
+        builder.Append(" MSIX Debug Build");
+    }
+
+    [Conditional("MSIX")]
+    private static void AppendMsixBuild(StringBuilder builder, ref bool done)
+    {
+        done = true;
+
+        builder.Append(" MSIX Build");
+    }
+
+    [Conditional("STEAM")]
+    private static void AppendSteamBuild(StringBuilder builder, ref bool done)
+    {
+        done = true;
+
+        builder.Append(" Steam Build");
     }
 }
