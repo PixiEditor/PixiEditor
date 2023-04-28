c09c4932be858eb5f974d4fc021eed33a572d8e4
CPKreuz <chrisphilkreuzgaming04@gmail.com>
2023-04-28 12:40:31 +0200

    Use command icons in main menu

--- src/PixiEditor/Models/Commands/XAML/Menu.cs
+++ src/PixiEditor/Models/Commands/XAML/Menu.cs
@@ -35,7 +35,23 @@ internal class Menu : System.Windows.Controls.Menu
 
         var command = CommandController.Current.Commands[value];
 
+        var icon = new Image 
+        { 
+            Source = command.GetIcon(), 
+            Width = 20, Height = 20,
+            Opacity = command.CanExecute() ? 1 : 0.75
+        };
+        
+        icon.IsVisibleChanged += (_, v) =>
+        {
+            if ((bool)v.NewValue)
+            {
+                icon.Opacity = command.CanExecute() ? 1 : 0.75;
+            }
+        };
+
         item.Command = Command.GetICommand(command, false);
+        item.Icon = icon;
         item.SetBinding(MenuItem.InputGestureTextProperty, ShortcutBinding.GetBinding(command));
     }
 
