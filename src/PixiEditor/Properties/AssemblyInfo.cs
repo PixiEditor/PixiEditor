4921b09402be3f6b2f64c8be31216670a20cee29
Krzysztof Krysiński <flubbet@gmail.com>
2023-05-07 16:13:30 +0200

    Updated Czech and Spanish, version++

--- src/PixiEditor/Properties/AssemblyInfo.cs
+++ src/PixiEditor/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("PixiEditor")]
-[assembly: AssemblyDescription("A lighweighted Pixel Art editor.")]
+[assembly: AssemblyDescription("A fast, nice looking pixel art editor.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("PixiEditor")]
 [assembly: AssemblyProduct("PixiEditor")]
@@ -50,5 +50,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
