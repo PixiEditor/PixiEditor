2d4678e369b3bb5eee6622d57336e5729ba85056
CPKreuz <chrisphilkreuzgaming04@gmail.com>
2023-05-11 15:39:37 +0200

    Changed DialogTitleBar.TitleText to TitleKey

--- src/PixiEditor/Views/Dialogs/DialogTitleBar.xaml.cs
+++ src/PixiEditor/Views/Dialogs/DialogTitleBar.xaml.cs
@@ -7,8 +7,8 @@ namespace PixiEditor.Views.Dialogs;
 
 internal partial class DialogTitleBar : UserControl
 {
-    public static readonly DependencyProperty TitleTextProperty =
-        DependencyProperty.Register(nameof(TitleText), typeof(string), typeof(DialogTitleBar), new PropertyMetadata(""));
+    public static readonly DependencyProperty TitleKeyProperty =
+        DependencyProperty.Register(nameof(TitleKey), typeof(string), typeof(DialogTitleBar), new PropertyMetadata(""));
 
     public static readonly DependencyProperty CloseCommandProperty =
         DependencyProperty.Register(nameof(CloseCommand), typeof(ICommand), typeof(DialogTitleBar), new PropertyMetadata(null));
@@ -19,10 +19,13 @@ internal partial class DialogTitleBar : UserControl
         set { SetValue(CloseCommandProperty, value); }
     }
 
-    public string TitleText
+    /// <summary>
+    /// The localization key of the window's title
+    /// </summary>
+    public string TitleKey
     {
-        get { return (string)GetValue(TitleTextProperty); }
-        set { SetValue(TitleTextProperty, value); }
+        get { return (string)GetValue(TitleKeyProperty); }
+        set { SetValue(TitleKeyProperty, value); }
     }
 
     public DialogTitleBar()
