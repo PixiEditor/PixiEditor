89474c3b2de791e2e7c3a93935ae7afd7c7ba5d9
Krzysztof Krysiński <flubbet@gmail.com>
2023-06-26 20:31:06 +0200

    Proper theming and a few controls implemented

--- src/PixiEditor.Avalonia/PixiEditor.Avalonia/App.axaml.cs
+++ src/PixiEditor.Avalonia/PixiEditor.Avalonia/App.axaml.cs
@@ -3,6 +3,7 @@ using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 using PixiEditor.Avalonia.ViewModels;
 using PixiEditor.Avalonia.Views;
+using PixiEditor.UI.Common.Themes;
 using Splat;
 
 namespace PixiEditor.Avalonia;
--- src/PixiEditor.UI.Common/Themes/PixiEditorTheme.cs
+++
@@ -1,9 +0,0 @@
-﻿using Avalonia.Styling;
-using Avalonia.Themes.Fluent;
-
-namespace PixiEditor.UI.Common.Themes;
-
-public class PixiEditorTheme : FluentTheme
-{
-
-}
