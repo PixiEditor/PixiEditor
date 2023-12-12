42b7d90e727714224590ae0b7048acea0273f2f3
Krzysztof Krysiński <flubbet@gmail.com>
2023-12-12 14:35:06 +0100

    Added toolbar and some brush reference fixes

---
+++ src/PixiEditor.AvaloniaUI/Views/Main/Tools/Toolbar.axaml.cs
@@ -0,0 +1,14 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace PixiEditor.AvaloniaUI.Views.Main.Tools;
+
+public partial class Toolbar : UserControl
+{
+    public Toolbar()
+    {
+        InitializeComponent();
+    }
+}
+
