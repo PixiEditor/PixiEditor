807c3d57ef450cb16e5d77e610ba8a1531bff540
Krzysztof Krysiński <flubbet@gmail.com>
2023-12-16 20:14:18 +0100

    Update version

--- src/PixiEditor/Properties/AssemblyInfo.cs
+++ src/PixiEditor/Properties/AssemblyInfo.cs
@@ -50,5 +50,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.1.3")]
-[assembly: AssemblyFileVersion("1.2.1.3")]
+[assembly: AssemblyVersion("1.2.1.4")]
+[assembly: AssemblyFileVersion("1.2.1.4")]
