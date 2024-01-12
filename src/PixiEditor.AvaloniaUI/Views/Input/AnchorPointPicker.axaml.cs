f988bde64423480ae91d5a42f7b448e49e6c3cde
Krzysztof Krysiński <flubbet@gmail.com>
2024-01-12 17:41:51 +0100

    Anchor picker wip

--- src/PixiEditor.AvaloniaUI/Views/Input/AnchorPointPicker.axaml.cs
+++ src/PixiEditor.AvaloniaUI/Views/Input/AnchorPointPicker.axaml.cs
@@ -21,12 +21,55 @@ internal partial class AnchorPointPicker : UserControl
         set => SetValue(AnchorPointProperty, value);
     }
 
+    static AnchorPointPicker()
+    {
+        AnchorPointProperty.Changed.Subscribe(OnAnchorPointChanged);
+    }
 
     private ToggleButton _selectedToggleButton;
 
     public AnchorPointPicker()
     {
         InitializeComponent();
+        _selectedToggleButton = topLeft;
+    }
+
+    private static void OnAnchorPointChanged(AvaloniaPropertyChangedEventArgs e)
+    {
+        AnchorPointPicker picker = (AnchorPointPicker)e.Sender;
+        if (e.NewValue is ResizeAnchor anchor)
+        {
+            switch (anchor)
+            {
+                case ResizeAnchor.TopLeft:
+                    picker.topLeft.IsChecked = true;
+                    break;
+                case ResizeAnchor.Top:
+                    picker.topCenter.IsChecked = true;
+                    break;
+                case ResizeAnchor.TopRight:
+                    picker.topRight.IsChecked = true;
+                    break;
+                case ResizeAnchor.Left:
+                    picker.middleLeft.IsChecked = true;
+                    break;
+                case ResizeAnchor.Center:
+                    picker.middleCenter.IsChecked = true;
+                    break;
+                case ResizeAnchor.Right:
+                    picker.middleRight.IsChecked = true;
+                    break;
+                case ResizeAnchor.BottomLeft:
+                    picker.bottomLeft.IsChecked = true;
+                    break;
+                case ResizeAnchor.Bottom:
+                    picker.bottomCenter.IsChecked = true;
+                    break;
+                case ResizeAnchor.BottomRight:
+                    picker.bottomRight.IsChecked = true;
+                    break;
+            }
+        }
     }
 
     private void ToggleButton_Checked(object sender, RoutedEventArgs e)
