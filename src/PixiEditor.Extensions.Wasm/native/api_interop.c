cbff63447bdc5d0b9c23143ed8e21d853c23b643
Krzysztof Krysiński <flubbet@gmail.com>
2024-01-22 23:28:17 +0100

    removed unused code

--- src/PixiEditor.Extensions.Wasm/native/api_interop.c
+++ src/PixiEditor.Extensions.Wasm/native/api_interop.c
@@ -24,20 +24,6 @@ void initialize()
     assert(!exception);
 }
 
-/*__attribute__((export_name("entry")))
-void entry()
-{
-    if (!method_entry) {
-        method_entry = lookup_dotnet_method("PixiEditor.Extensions.Wasm.dll", "PixiEditor.Extensions.Wasm", "Interop", "Entry", -1);
-        assert(method_entry);
-    }
-
-    void* method_params[] = {};
-    MonoObject *exception;
-    mono_wasm_invoke_method (method_entry, NULL, method_params, &exception);
-    assert (!exception);
-}*/
-
 void logger_log_message(MonoString* message)
 {
     char* message_utf8 = mono_wasm_string_get_utf8(message);
