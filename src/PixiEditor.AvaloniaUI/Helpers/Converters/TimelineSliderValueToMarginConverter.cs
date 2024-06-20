ff32c892ea4669a422c2960070420967416cb161
flabbet <flubbet@gmail.com>
2024-06-20 13:05:32 +0200

    Active frame Line

---
+++ src/PixiEditor.AvaloniaUI/Helpers/Converters/TimelineSliderValueToMarginConverter.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+using Avalonia;
+
+namespace PixiEditor.AvaloniaUI.Helpers.Converters;
+
+internal class TimelineSliderValueToMarginConverter : SingleInstanceMultiValueConverter<TimelineSliderValueToMarginConverter>
+{
+    public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return Convert(new List<object?> { value }, targetType, parameter, culture);
+    }
+
+    public override object? Convert(IList<object?> values, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (values.Count != 3)
+        {
+            throw new ArgumentException("TimelineSliderValueToMarginConverter requires 3 values");
+        }
+
+        if (values[0] is int frame && values[1] is double scale && values[2] is Vector offset)
+        {
+            return new Thickness(frame * scale + offset.X, 0, 0, 0);
+        }
+
+        return new Thickness();
+    }
+}
