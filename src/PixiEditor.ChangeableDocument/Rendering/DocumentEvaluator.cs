5ee065f692b1600e1d61c0f14ddb67f09d110b46
flabbet <flubbet@gmail.com>
2024-07-15 19:13:49 +0200

    Fixed rendering on bigger sizes

--- src/PixiEditor.ChangeableDocument/Rendering/DocumentEvaluator.cs
+++ src/PixiEditor.ChangeableDocument/Rendering/DocumentEvaluator.cs
@@ -30,12 +30,14 @@ public static class DocumentEvaluator
                 chunk.Surface.DrawingSurface.Canvas.ClipRect((RectD)transformedClippingRect);
             }
             
-            VecD pos = chunkPos * resolution.Multiplier();
+            VecD pos = chunkPos;
             int x = (int)(pos.X * ChunkyImage.FullChunkSize);
             int y = (int)(pos.Y * ChunkyImage.FullChunkSize);
+            int width = ChunkyImage.FullChunkSize;
+            int height = ChunkyImage.FullChunkSize;
             
-            RectD sourceRect = new(x, y, ChunkyImage.FullChunkSize, ChunkyImage.FullChunkSize);
-            RectD destRect = new(0, 0, ChunkyImage.FullChunkSize, ChunkyImage.FullChunkSize);
+            RectD sourceRect = new(x, y, width, height);
+            RectD destRect = new(0, 0, chunk.PixelSize.X, chunk.PixelSize.Y);
             
             chunk.Surface.DrawingSurface.Canvas.DrawImage(evaluated, sourceRect, destRect, context.ReplacingPaintWithOpacity);
 
