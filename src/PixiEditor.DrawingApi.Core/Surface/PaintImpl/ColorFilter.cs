3175ed7520ce5340ae30fb9ba2d9046a4a9aa214
CPKreuz <cpkreu@gmail.com>
2024-07-16 17:59:18 +0200

    Added ColorFilter.CreateColorMatrix to drawing backend

--- src/PixiEditor.DrawingApi.Core/Surface/PaintImpl/ColorFilter.cs
+++ src/PixiEditor.DrawingApi.Core/Surface/PaintImpl/ColorFilter.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using PixiEditor.DrawingApi.Core.Bridge;
 using PixiEditor.DrawingApi.Core.ColorsImpl;
+using PixiEditor.Numerics;
 
 namespace PixiEditor.DrawingApi.Core.Surface.PaintImpl;
 
@@ -16,6 +17,23 @@ public class ColorFilter : NativeObject
     {
         ColorFilter filter = new ColorFilter(DrawingBackendApi.Current.ColorFilterImplementation.CreateBlendMode(color, blendMode));
         return filter;
+    }
+
+    /// <param name="matrix">An array of <see cref="F:SkiaSharp.SKColorFilter.ColorMatrixSize" /> elements.</param>
+    /// <summary>Creates a new color filter that transforms a color by a 4x5 (row-major) matrix.</summary>
+    /// <returns>Returns the new <see cref="T:PixiEditor.DrawingApi.Core.Surface.PaintImpl.ColorFilter" />.</returns>
+    /// <remarks>The matrix is in row-major order and the translation column is specified in unnormalized, 0...255, space.</remarks>
+    public static ColorFilter CreateColorMatrix(float[] matrix)
+    {
+        return new ColorFilter(DrawingBackendApi.Current.ColorFilterImplementation.CreateColorMatrix(matrix));
+    }
+
+    public static ColorFilter CreateColorMatrix(Matrix4x5F matrix)
+    {
+        float[] values = new float[Matrix4x5F.Width * Matrix4x5F.Height];
+        matrix.TryGetMembers(values);
+
+        return CreateColorMatrix(values);
     }
 
     public override void Dispose()
