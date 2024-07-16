52e7772cf5fccf4732c25c5a80c9dfe4d7a37e9c
CPKreuz <cpkreu@gmail.com>
2024-07-16 23:51:28 +0200

    Added VecD and int (Int32) property views

---
+++ src/PixiEditor.AvaloniaUI/ViewModels/Nodes/Properties/VecDPropertyViewModel.cs
@@ -0,0 +1,10 @@
+﻿using PixiEditor.Numerics;
+
+namespace PixiEditor.AvaloniaUI.ViewModels.Nodes.Properties;
+
+internal class VecDPropertyViewModel : NodePropertyViewModel<VecD>
+{
+    public VecDPropertyViewModel(NodeViewModel node, Type valueType) : base(node, valueType)
+    {
+    }
+}
