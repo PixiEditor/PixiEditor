19a21a1109b7287253bd947dfaa35cbabe6a0054
flabbet <flubbet@gmail.com>
2024-07-31 13:22:48 +0200

    Piped ffmpeg rendering

--- src/PixiEditor.DrawingApi.Core/Bridge/NativeObjectsImpl/IBitmapImplementation.cs
+++ src/PixiEditor.DrawingApi.Core/Bridge/NativeObjectsImpl/IBitmapImplementation.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using PixiEditor.DrawingApi.Core.Surfaces;
+using PixiEditor.DrawingApi.Core.Surfaces.ImageData;
 using PixiEditor.Numerics;
 
 namespace PixiEditor.DrawingApi.Core.Bridge.NativeObjectsImpl;
@@ -11,4 +12,6 @@ public interface IBitmapImplementation
     public object GetNativeBitmap(IntPtr objectPointer);
     public Bitmap FromImage(IntPtr snapshot);
     public VecI GetSize(IntPtr objectPointer);
+    public byte[] GetBytes(IntPtr objectPointer);
+    public ImageInfo GetInfo(IntPtr objectPointer);
 }
