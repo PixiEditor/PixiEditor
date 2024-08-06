60865bbb9d99d28debe76494f58cf80bb517722b
CPKreuz <cpkreu@gmail.com>
2024-08-06 16:39:53 +0200

    Remove unused event types

--- src/PixiEditor/Models/AnalyticsAPI/AnalyticEventTypes.cs
+++ src/PixiEditor/Models/AnalyticsAPI/AnalyticEventTypes.cs
@@ -4,11 +4,9 @@ public class AnalyticEventTypes
 {
     public static string Startup { get; } = GetEventType("Startup");
     public static string CreateDocument { get; } = GetEventType("CreateDocument");
-    public static string OpenWindow { get; } = GetEventType("OpenWindow");
     public static string CreateNode { get; } = GetEventType("CreateNode");
     public static string CreateKeyframe { get; } = GetEventType("CreateKeyframe");
     public static string CloseDocument { get; } = GetEventType("CloseDocument");
-    public static string ResizeDocument { get; } = GetEventType("ResizeDocument");
     public static string OpenExample { get; } = GetEventType("OpenExample");
     public static string OpenFile { get; } = GetEventType("OpenFile");
     public static string GeneralCommand { get; } = GetEventType("GeneralCommand");
