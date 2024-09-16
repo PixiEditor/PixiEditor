ebe88842f8354414de812938d84f734351e7496a
flabbet <flubbet@gmail.com>
2024-09-16 15:34:28 +0200

    Fixed zoom tool trying to create null layer

--- src/PixiEditor/ViewModels/Tools/Tools/ZoomToolViewModel.cs
+++ src/PixiEditor/ViewModels/Tools/Tools/ZoomToolViewModel.cs
@@ -20,7 +20,7 @@ internal class ZoomToolViewModel : ToolViewModel
 
     public override string ToolNameLocalizationKey => "ZOOM_TOOL";
     public override BrushShape BrushShape => BrushShape.Hidden;
-    public override Type[]? SupportedLayerTypes { get; } = [];
+    public override Type[]? SupportedLayerTypes { get; } = null;
 
     public override bool StopsLinkedToolOnUse => false;
 
