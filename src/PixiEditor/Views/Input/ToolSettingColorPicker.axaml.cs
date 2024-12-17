0b1b214eed4617eca72475f2d06c3972385e9c9d
flabbet <flubbet@gmail.com>
2024-12-17 11:53:09 +0100

    Removed keyboard focus from toolbar settings

--- src/PixiEditor/Views/Input/ToolSettingColorPicker.axaml.cs
+++ src/PixiEditor/Views/Input/ToolSettingColorPicker.axaml.cs
@@ -1,25 +1,34 @@
 ﻿using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
 using Avalonia.Media;
 using Avalonia.Threading;
+using Avalonia.VisualTree;
+using PixiEditor.Helpers.Behaviours;
 
 namespace PixiEditor.Views.Input;
 
 internal partial class ToolSettingColorPicker : UserControl
 {
-    public static readonly StyledProperty<Color> SelectedColorProperty = AvaloniaProperty.Register<ToolSettingColorPicker, Color>(
-        nameof(SelectedColor));
+    public static readonly StyledProperty<Color> SelectedColorProperty =
+        AvaloniaProperty.Register<ToolSettingColorPicker, Color>(
+            nameof(SelectedColor));
 
     public Color SelectedColor
     {
         get => GetValue(SelectedColorProperty);
         set => SetValue(SelectedColorProperty, value);
     }
-    
+
     public ToolSettingColorPicker()
     {
         InitializeComponent();
         ColorPicker.SecondaryColor = Colors.Black;
+        ColorPicker.TemplateApplied += ColorPickerOnTemplateApplied;
+    }
+
+    private void ColorPickerOnTemplateApplied(object? sender, TemplateAppliedEventArgs e)
+    {
+        ColorPicker.FindDescendantOfType<ToggleButton>().Focusable = false;
     }
 }
-
