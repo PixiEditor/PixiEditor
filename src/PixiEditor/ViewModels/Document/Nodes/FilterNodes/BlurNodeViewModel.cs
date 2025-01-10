9c90bffd1c85df5bab31fc416e6442c0bc9d8233
flabbet <flubbet@gmail.com>
2025-01-10 17:04:32 +0100

    Added blur icon and changed node name in english

--- src/PixiEditor/ViewModels/Document/Nodes/FilterNodes/BlurNodeViewModel.cs
+++ src/PixiEditor/ViewModels/Document/Nodes/FilterNodes/BlurNodeViewModel.cs
@@ -3,5 +3,5 @@ using PixiEditor.ViewModels.Nodes;
 
 namespace PixiEditor.ViewModels.Document.Nodes.FilterNodes;
 
-[NodeViewModel("BLUR_FILTER_NODE", "FILTERS", null)] // TODO: Blur Icon
+[NodeViewModel("BLUR_FILTER_NODE", "FILTERS", "\ue822")]
 internal class BlurNodeViewModel : NodeViewModel<BlurNode>;
