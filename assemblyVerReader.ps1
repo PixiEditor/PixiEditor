f1c721eecebcd208adacf2c756ad7419818da2e1
flabbet <flubbet@gmail.com>
2025-01-20 15:02:30 +0100

    Removed assembly ver reader

--- assemblyVerReader.ps1
+++
@@ -1,6 +0,0 @@
-$assemblyInfoPath = "src\PixiEditor\Properties\AssemblyInfo.cs"
-
-$contents = [System.IO.File]::ReadAllText($assemblyInfoPath)
-
-$versionString = [RegEx]::Match($contents,"(?:\d+\.\d+\.\d+\.\d+)")
-Write-Host "##vso[task.setvariable variable=TagVersion;]$versionString"
