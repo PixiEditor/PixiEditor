ad8db31a2ba17ab4f7ee54998df9c6227947c86c
Krzysztof Krysiński <flubbet@gmail.com>
2025-02-24 09:43:27 +0100

    Added drop shadow

---
+++ src/PixiEditor/ViewModels/Document/Nodes/FilterNodes/ShadowNodeViewModel.cs
@@ -0,0 +1,9 @@
+﻿using PixiEditor.ChangeableDocument.Changeables.Graph.Nodes.FilterNodes;
+using PixiEditor.ViewModels.Nodes;
+
+namespace PixiEditor.ViewModels.Document.Nodes.FilterNodes;
+
+[NodeViewModel("SHADOW_NODE", "FILTERS", "\ue812")]
+internal class ShadowNodeViewModel : NodeViewModel<ShadowNode>
+{
+}
