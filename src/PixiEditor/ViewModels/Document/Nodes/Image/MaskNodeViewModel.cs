f098be91cf857333b64ba882fe271e4dce5b736b
Krzysztof Krysiński <flubbet@gmail.com>
2025-02-26 09:42:45 +0100

    Added mask node

---
+++ src/PixiEditor/ViewModels/Document/Nodes/Image/MaskNodeViewModel.cs
@@ -0,0 +1,8 @@
+﻿using PixiEditor.ChangeableDocument.Changeables.Graph.Nodes.Image;
+using PixiEditor.UI.Common.Fonts;
+using PixiEditor.ViewModels.Nodes;
+
+namespace PixiEditor.ViewModels.Document.Nodes.Image;
+
+[NodeViewModel("MASK_NODE", "IMAGE", PixiPerfectIcons.CreateMask)]
+internal class MaskNodeViewModel : NodeViewModel<MaskNode>;
