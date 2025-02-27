4c2a8e2b8b39e2bb28c8e8ff0162305f2d6f8ca8
Krzysztof Krysiński <flubbet@gmail.com>
2025-02-27 11:58:46 +0100

    Color Adjustments filter wip

---
+++ src/PixiEditor/ViewModels/Document/Nodes/FilterNodes/ColorAdjustmentsFilterNodeViewModel.cs
@@ -0,0 +1,6 @@
+﻿using PixiEditor.ViewModels.Nodes;
+
+namespace PixiEditor.ViewModels.Document.Nodes.FilterNodes;
+
+[NodeViewModel("COLOR_ADJUSTMENTS_FILTER", "FILTERS", "")]
+internal class ColorAdjustmentsFilterNodeViewModel : NodeViewModel<ColorAdjustmentsFilterNode>;
