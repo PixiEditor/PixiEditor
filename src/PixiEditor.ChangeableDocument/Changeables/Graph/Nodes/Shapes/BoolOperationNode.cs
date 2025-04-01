9f131cb62f5d97b545c6278019a68db39eaf5ec1
Krzysztof Krysiński <flubbet@gmail.com>
2025-04-01 19:48:49 +0200

    Added boolean operation node

---
+++ src/PixiEditor.ChangeableDocument/Changeables/Graph/Nodes/Shapes/BoolOperationNode.cs
@@ -0,0 +1,61 @@
+﻿using Drawie.Backend.Core.Vector;
+using PixiEditor.ChangeableDocument.Changeables.Graph.Nodes.Shapes.Data;
+using PixiEditor.ChangeableDocument.Rendering;
+
+namespace PixiEditor.ChangeableDocument.Changeables.Graph.Nodes.Shapes;
+
+[NodeInfo("BoolOperation")]
+public class BoolOperationNode : Node
+{
+    public InputProperty<ShapeVectorData> ShapeA { get; }
+    public InputProperty<ShapeVectorData> ShapeB { get; }
+    public InputProperty<VectorPathOp> Operation { get; }
+    public OutputProperty<ShapeVectorData> Result { get; }
+
+    protected override bool ExecuteOnlyOnCacheChange => true;
+
+    public BoolOperationNode()
+    {
+        ShapeA = CreateInput<ShapeVectorData>("ShapeA", "FIRST_SHAPE", null);
+        ShapeB = CreateInput<ShapeVectorData>("ShapeB", "SECOND_SHAPE", null);
+        Operation = CreateInput<VectorPathOp>("Operation", "OPERATION", VectorPathOp.Union);
+        Result = CreateOutput<ShapeVectorData>("Result", "RESULT", null);
+    }
+
+    protected override void OnExecute(RenderContext context)
+    {
+        if (ShapeA.Value == null && ShapeB.Value == null)
+        {
+            Result.Value = null;
+            return;
+        }
+
+        if (ShapeA.Value == null)
+        {
+            Result.Value = ShapeB.Value;
+            return;
+        }
+
+        if (ShapeB.Value == null)
+        {
+            Result.Value = ShapeA.Value;
+            return;
+        }
+
+        ShapeVectorData shapeA = ShapeA.Value;
+        ShapeVectorData shapeB = ShapeB.Value;
+
+        Result.Value = new PathVectorData(shapeA.ToPath(true).Op(shapeB.ToPath(true), Operation.Value))
+        {
+            Fill = shapeA.Fill,
+            Stroke = shapeA.Stroke,
+            StrokeWidth = shapeA.StrokeWidth,
+            FillPaintable = shapeA.FillPaintable,
+        };
+    }
+
+    public override Node CreateCopy()
+    {
+        return new BoolOperationNode();
+    }
+}
---
+++ src/PixiEditor/ViewModels/Document/Nodes/Shapes/BoolOperationNodeViewModel.cs
@@ -0,0 +1,11 @@
+﻿using PixiEditor.ChangeableDocument.Changeables.Graph.Nodes.Shapes;
+using PixiEditor.UI.Common.Fonts;
+using PixiEditor.ViewModels.Nodes;
+
+namespace PixiEditor.ViewModels.Document.Nodes.Shapes;
+
+[NodeViewModel("BOOL_OPERATION_NODE", "SHAPE", PixiPerfectIcons.Intersect)]
+internal class BoolOperationNodeViewModel : NodeViewModel<BoolOperationNode>
+{
+
+}
