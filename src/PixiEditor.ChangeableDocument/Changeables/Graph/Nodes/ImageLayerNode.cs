41689aee2da7ffb70d69686f7085252b88ce9b13
Krzysztof Krysiński <flubbet@gmail.com>
2025-05-01 16:51:14 +0200

    Fixed blending

--- src/PixiEditor.ChangeableDocument/Changeables/Graph/Nodes/ImageLayerNode.cs
+++ src/PixiEditor.ChangeableDocument/Changeables/Graph/Nodes/ImageLayerNode.cs
@@ -62,13 +62,13 @@ public class ImageLayerNode : LayerNode, IReadOnlyImageNode
 
     protected internal override void DrawLayerOnTexture(SceneObjectRenderContext ctx,
         DrawingSurface workingSurface,
-        bool useFilters)
+        bool useFilters, Paint paint)
     {
         int scaled = workingSurface.Canvas.Save();
         workingSurface.Canvas.Translate(GetScenePosition(ctx.FrameTime) * ctx.ChunkResolution.Multiplier());
         workingSurface.Canvas.Scale((float)ctx.ChunkResolution.Multiplier());
 
-        DrawLayerOnto(ctx, workingSurface, useFilters);
+        DrawLayerOnto(ctx, workingSurface, useFilters, paint);
 
         workingSurface.Canvas.RestoreToCount(scaled);
     }
