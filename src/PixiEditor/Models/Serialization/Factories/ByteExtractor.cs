239b85c2d6a1ee89c81963361d8d31619ede0c58
Krzysztof Krysiński <flubbet@gmail.com>
2025-05-24 09:33:33 +0200

    Fixed byte reader not reading strings properly

--- src/PixiEditor/Models/Serialization/Factories/ByteExtractor.cs
+++ src/PixiEditor/Models/Serialization/Factories/ByteExtractor.cs
@@ -86,14 +86,10 @@ public class ByteExtractor
     public string GetString()
     {
         int length = GetInt();
-        StringBuilder builder = new StringBuilder();
-        
-        for (int i = 0; i < length; i++)
-        {
-            builder.Append((char)GetInt());
-        }
-        
-        return builder.ToString();
+        string value = Encoding.UTF8.GetString(_data, Position, length);
+
+        Position += length;
+        return value;
     }
 
     public float GetFloat()
