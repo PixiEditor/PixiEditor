d5f84de87b1393df4a3936b852467be87f178cca
Krzysztof Krysiński <flubbet@gmail.com>
2025-06-16 13:55:03 +0200

    Text tool click and drag

--- src/PixiEditor/Models/DocumentModels/UpdateableChangeExecutors/VectorTextToolExecutor.cs
+++ src/PixiEditor/Models/DocumentModels/UpdateableChangeExecutors/VectorTextToolExecutor.cs
@@ -34,6 +34,9 @@ internal class VectorTextToolExecutor : UpdateableChangeExecutor, ITextOverlayEv
     private bool isListeningForValidLayer;
     private VectorPath? onPath;
 
+    private VecD clickPos;
+    private bool wasDrawingSize;
+
     private List<Font> fontsToDispose = new();
 
     public override bool BlocksOtherActions => false;
@@ -98,6 +101,7 @@ internal class VectorTextToolExecutor : UpdateableChangeExecutor, ITextOverlayEv
                 Matrix3X3.Identity, toolbar.Spacing);
             lastText = "";
             position = controller.LastPrecisePosition;
+            clickPos = controller.LastPrecisePosition;
             // TODO: Implement proper putting on path editing
             /*if (controller.LeftMousePressed)
             {
@@ -116,6 +120,7 @@ internal class VectorTextToolExecutor : UpdateableChangeExecutor, ITextOverlayEv
     {
         var topMostWithinClick = QueryLayers<IVectorLayerHandler>(args.PositionOnCanvas);
 
+        clickPos = args.PositionOnCanvas;
         var firstLayer = topMostWithinClick.FirstOrDefault();
         args.Handled = firstLayer != null;
         if (firstLayer is not IVectorLayerHandler layerHandler)
@@ -141,6 +146,29 @@ internal class VectorTextToolExecutor : UpdateableChangeExecutor, ITextOverlayEv
 
                 document.TextOverlayHandler.SetCursorPosition(args.PositionOnCanvas);
             }, false);
+    }
+
+    public override void OnPrecisePositionChange(VecD pos)
+    {
+        if (document.TextOverlayHandler.IsActive && internals.ChangeController.LeftMousePressed && string.IsNullOrEmpty(lastText))
+        {
+            wasDrawingSize = true;
+            double distance = Math.Abs(clickPos.Y - pos.Y);
+            position = new VecD(position.X, pos.Y);
+            document.TextOverlayHandler.Position = position;
+            document.TextOverlayHandler.PreviewSize = true;
+            var textData = ConstructTextData(lastText);
+            toolbar.FontSize = distance * RichText.PtToPx;
+            internals.ActionAccumulator.AddActions(new SetShapeGeometry_Action(selectedMember.Id, textData, VectorShapeChangeType.GeometryData));
+        }
+    }
+
+    public override void OnLeftMouseButtonUp(VecD pos)
+    {
+        if (wasDrawingSize)
+        {
+            document.TextOverlayHandler.PreviewSize = false;
+        }
     }
 
     public void OnQuickToolSwitch()
--- src/PixiEditor/ViewModels/Document/Nodes/Calculations/RemapNodeViewModel.cs
+++ src/PixiEditor/ViewModels/Document/Nodes/Calculations/RemapNodeViewModel.cs
@@ -2,7 +2,7 @@
 
 namespace PixiEditor.ViewModels.Document.Nodes.Calculations;
 
-[NodeViewModel("REMAP_NODE", "NUMBERS", "")]
+[NodeViewModel("REMAP_NODE", "NUMBERS", PixiPerfectIcons.Map)]
 internal class RemapNodeViewModel : NodeViewModel<PixiEditor.ChangeableDocument.Changeables.Graph.Nodes.Calculations.RemapNode>
 {
 
