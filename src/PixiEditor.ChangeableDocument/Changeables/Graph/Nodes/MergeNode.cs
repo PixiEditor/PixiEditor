1d0890fc7c4ca6fa887de8aedd56e572c6e7e13d
Krzysztof Krysiński <flubbet@gmail.com>
2025-06-16 17:39:19 +0200

    reverted optimization that breaks things

--- src/PixiEditor.ChangeableDocument/Changeables/Graph/Nodes/MergeNode.cs
+++ src/PixiEditor.ChangeableDocument/Changeables/Graph/Nodes/MergeNode.cs
@@ -52,7 +52,7 @@ public class MergeNode : RenderNode
 
     private void Merge(DrawingSurface target, RenderContext context)
     {
-        if (Bottom.Value != null && Top.Value != null && BlendMode.Value != Enums.BlendMode.Normal)
+        if (Bottom.Value != null && Top.Value != null)
         {
             int saved = target.Canvas.SaveLayer();
             Bottom.Value?.Paint(context, target);
