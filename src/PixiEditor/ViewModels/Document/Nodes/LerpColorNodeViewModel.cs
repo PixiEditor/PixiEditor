f42350799eb1dbda4c41b04f2f632f8f59a13cd5
Krzysztof Krysiński <flubbet@gmail.com>
2025-06-23 18:54:38 +0200

    Update LerpColorNodeViewModel.cs

--- src/PixiEditor/ViewModels/Document/Nodes/LerpColorNodeViewModel.cs
+++ src/PixiEditor/ViewModels/Document/Nodes/LerpColorNodeViewModel.cs
@@ -4,5 +4,5 @@ using PixiEditor.ViewModels.Nodes;
 
 namespace PixiEditor.ViewModels.Document.Nodes;
 
-[NodeViewModel("LERP_NODE", "NUMBERS", PixiPerfectIcons.SunCold)]
+[NodeViewModel("LERP_NODE", "COLOR", PixiPerfectIcons.SunCold)]
 internal class LerpColorNodeViewModel : NodeViewModel<LerpColorNode>;
