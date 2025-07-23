661d135b3ef1151226e8c3ed734cea36f0681e6f
CPKreuz <cpkreu@gmail.com>
2025-07-23 21:02:07 +0200

    Added enum Descriptions for first party enums

--- src/PixiEditor/Models/Handlers/Toolbars/PaintBrushShape.cs
+++ src/PixiEditor/Models/Handlers/Toolbars/PaintBrushShape.cs
@@ -1,7 +1,11 @@
-﻿namespace PixiEditor.Models.Handlers.Toolbars;
+﻿using System.ComponentModel;
+
+namespace PixiEditor.Models.Handlers.Toolbars;
 
 public enum PaintBrushShape
 {
+    [Description("PAINT_BRUSH_SHAPE_CIRCLE")]
     Circle,
+    [Description("PAINT_BRUSH_SHAPE_SQUARE")]
     Square,
 }
