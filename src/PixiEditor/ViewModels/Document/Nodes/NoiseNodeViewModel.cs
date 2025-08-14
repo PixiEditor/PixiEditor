0d58a01bb98be7c2fa8733e07dc5abe8e26847cf
Jakub Ciemała <ciemalajakub@gmail.com>
2025-08-14 21:36:01 +0200

    Show randomness input only for Voronoi noise type

--- src/PixiEditor/ViewModels/Document/Nodes/NoiseNodeViewModel.cs
+++ src/PixiEditor/ViewModels/Document/Nodes/NoiseNodeViewModel.cs
@@ -1,8 +1,30 @@
 using PixiEditor.ChangeableDocument.Changeables.Graph.Nodes;
 using PixiEditor.UI.Common.Fonts;
 using PixiEditor.ViewModels.Nodes;
+using PixiEditor.ViewModels.Nodes.Properties;
 
 namespace PixiEditor.ViewModels.Document.Nodes;
 
 [NodeViewModel("NOISE_NODE", "IMAGE", PixiPerfectIcons.Noise)]
-internal class NoiseNodeViewModel : NodeViewModel<NoiseNode>;
+internal class NoiseNodeViewModel : NodeViewModel<NoiseNode>
+{
+    private GenericEnumPropertyViewModel Type { get; set; }
+    private NodePropertyViewModel Randomness { get; set; }
+
+    public override void OnInitialized()
+    {
+        Type = FindInputProperty("NoiseType") as GenericEnumPropertyViewModel;
+        Randomness = FindInputProperty("Randomness");
+
+        Type.ValueChanged += (_, _) => UpdateInputVisibility();
+        UpdateInputVisibility();
+    }
+
+    private void UpdateInputVisibility()
+    {
+        if (Type.Value is not NoiseType type)
+            return;
+        
+        Randomness.IsVisible = type == NoiseType.Voronoi;
+    }
+}
