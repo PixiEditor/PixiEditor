9bd59e97431c80421859efdee8bbd799634d47b6
CPKreuz <cpkreu@gmail.com>
2025-08-23 19:50:27 +0200

    Added a text info node

---
+++ src/PixiEditor.ChangeableDocument/Changeables/Graph/Nodes/Text/TextInfoNode.cs
@@ -0,0 +1,32 @@
+﻿using PixiEditor.ChangeableDocument.Rendering;
+
+namespace PixiEditor.ChangeableDocument.Changeables.Graph.Nodes.Text;
+
+[NodeInfo("TextInfo")]
+public class TextInfoNode : Node
+{
+    public OutputProperty<int> Length { get; }
+    
+    public OutputProperty<int> LineCount { get; }
+    
+    public InputProperty<string> Text { get; }
+
+    public TextInfoNode()
+    {
+        Length = CreateOutput("Length", "TEXT_LENGTH", 0);
+        LineCount = CreateOutput("LineCount", "TEXT_LINE_COUNT", 0);
+        
+        Text = CreateInput("Text", "TEXT", string.Empty);
+    }
+    
+    protected override void OnExecute(RenderContext context)
+    {
+        var text = Text.Value;
+        
+        Length.Value = text.Length;
+        LineCount.Value = text.AsSpan().Count('\n');
+    }
+
+    public override Node CreateCopy() =>
+        new TextInfoNode();
+}
---
+++ src/PixiEditor/ViewModels/Document/Nodes/Text/TextInfoNodeViewModel.cs
@@ -0,0 +1,7 @@
+﻿using PixiEditor.ChangeableDocument.Changeables.Graph.Nodes.Text;
+using PixiEditor.ViewModels.Nodes;
+
+namespace PixiEditor.ViewModels.Document.Nodes.Text;
+
+[NodeViewModel("TEXT_INFO_NODE", "SHAPE", null)]
+internal class TextInfoNodeViewModel : NodeViewModel<TextInfoNode>;
