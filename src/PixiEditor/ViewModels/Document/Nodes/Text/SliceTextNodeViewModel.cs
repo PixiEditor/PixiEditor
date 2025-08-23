c782a0dcf7175339ad17e138c8f264577640f832
CPKreuz <cpkreu@gmail.com>
2025-08-23 19:42:31 +0200

    Improve slice text node localization key

--- src/PixiEditor/ViewModels/Document/Nodes/Text/SliceTextNodeViewModel.cs
+++ src/PixiEditor/ViewModels/Document/Nodes/Text/SliceTextNodeViewModel.cs
@@ -3,5 +3,5 @@ using PixiEditor.ViewModels.Nodes;
 
 namespace PixiEditor.ViewModels.Document.Nodes.Text;
 
-[NodeViewModel("SLICE_TEXT", "SHAPE", null)]
+[NodeViewModel("SLICE_TEXT_NODE", "SHAPE", null)]
 internal class SliceTextNodeViewModel : NodeViewModel<SliceTextNode>;
