f0aa49e1084a9fdf95c6c1855b8c98f4d4b4b386
Jakub Ciemała <ciemalajakub@gmail.com>
2025-09-05 11:52:47 +0200

    Change posterization node icon

--- src/PixiEditor/ViewModels/Document/Nodes/Effects/PosterizationNodeViewModel.cs
+++ src/PixiEditor/ViewModels/Document/Nodes/Effects/PosterizationNodeViewModel.cs
@@ -3,7 +3,7 @@ using PixiEditor.ViewModels.Nodes;
 
 namespace PixiEditor.ViewModels.Document.Nodes.Effects;
 
-[NodeViewModel("POSTERIZATION_NODE", "EFFECTS", PixiPerfectIcons.ColorPalette)]
+[NodeViewModel("POSTERIZATION_NODE", "EFFECTS", PixiPerfectIcons.Swatches)]
 internal class PosterizationNodeViewModel : NodeViewModel<PosterizationNode>
 {
     
