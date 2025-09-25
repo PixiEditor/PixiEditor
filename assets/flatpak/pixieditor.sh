142de7e5e943229dc3164b70b4a0d61a2dda267c
Krzysztof Krysiński <flubbet@gmail.com>
2025-09-25 14:11:43 +0200

    Moved to other repo

--- assets/flatpak/pixieditor.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-# Find flatpak even if PATH is not set properly
-FLATPAK_BIN="$(command -v flatpak || echo /usr/bin/flatpak)"
-exec "$FLATPAK_BIN" run --file-forwarding net.pixieditor.PixiEditor @@ "$@" @@
