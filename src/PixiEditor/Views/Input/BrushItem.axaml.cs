f4d116258cf6dd9c879c4b526b38e0827df5e7a3
Krzysztof Krysiński <flubbet@gmail.com>
2025-11-17 16:28:16 +0100

    Basic Brush Picker works!

---
+++ src/PixiEditor/Views/Input/BrushItem.axaml.cs
@@ -0,0 +1,24 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Metadata;
+using Avalonia.Markup.Xaml;
+using PixiEditor.Models.BrushEngine;
+
+namespace PixiEditor.Views.Input;
+
+internal partial class BrushItem : UserControl
+{
+    public static readonly StyledProperty<Brush> BrushProperty = AvaloniaProperty.Register<BrushItem, Brush>("Brush");
+
+    public BrushItem()
+    {
+        InitializeComponent();
+    }
+
+    public Brush Brush
+    {
+        get { return (Brush)GetValue(BrushProperty); }
+        set { SetValue(BrushProperty, value); }
+    }
+}
+
--- src/PixiEditor/Views/Input/BrushPicker.axaml.cs
+++ src/PixiEditor/Views/Input/BrushPicker.axaml.cs
@@ -4,21 +4,13 @@ using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 using Avalonia.Media;
 using Avalonia.VisualTree;
+using CommunityToolkit.Mvvm.Input;
 using Brush = PixiEditor.Models.BrushEngine.Brush;
 
 namespace PixiEditor.Views.Input;
 
 internal partial class BrushPicker : UserControl
 {
-    public static readonly StyledProperty<int> BrushIndexProperty = AvaloniaProperty.Register<BrushPicker, int>(
-        nameof(BrushIndex));
-
-    public int BrushIndex
-    {
-        get => GetValue(BrushIndexProperty);
-        set => SetValue(BrushIndexProperty, value);
-    }
-
     public static readonly StyledProperty<ObservableCollection<Brush>> BrushesProperty =
         AvaloniaProperty.Register<BrushPicker, ObservableCollection<Brush>>(
             nameof(Brushes));
@@ -39,23 +31,13 @@ internal partial class BrushPicker : UserControl
         set => SetValue(SelectedBrushProperty, value);
     }
 
-    private bool suppressUpdate = false;
-
     static BrushPicker()
     {
-        BrushesProperty.Changed.AddClassHandler<BrushPicker>((x, e) => x.OnBrushesChanged(e));
-        BrushIndexProperty.Changed.AddClassHandler<BrushPicker>((x, e) => x.OnBrushIndexChanged(e));
-        SelectedBrushProperty.Changed.AddClassHandler<BrushPicker>((x, e) =>
+        BrushesProperty.Changed.AddClassHandler<BrushPicker>((x, e) =>
         {
-            if (e.NewValue is Brush newBrush && x.Brushes != null)
+            if (x.SelectedBrush == null && x.Brushes.Count > 0)
             {
-                int index = x.Brushes.IndexOf(newBrush);
-                if (index != -1 && x.BrushIndex != index)
-                {
-                    x.suppressUpdate = true;
-                    x.BrushIndex = index;
-                    x.suppressUpdate = false;
-                }
+                x.SelectedBrush = x.Brushes[0];
             }
         });
     }
@@ -65,43 +47,11 @@ internal partial class BrushPicker : UserControl
         InitializeComponent();
     }
 
-    private void OnBrushesChanged(AvaloniaPropertyChangedEventArgs e)
+    [RelayCommand]
+    public void SelectBrush(Brush brush)
     {
-        if (e.NewValue is ObservableCollection<Brush> brushes && brushes.Count > 0)
-        {
-            BrushIndex = brushes.IndexOf(SelectedBrush);
-            if (BrushIndex == -1)
-            {
-                BrushIndex = 0;
-                SelectedBrush = brushes[0];
-            }
-        }
-    }
-
-    protected override void OnAttachedToVisualTree(VisualTreeAttachmentEventArgs e)
-    {
-        if(SelectedBrush != null && BrushIndex == -1 && Brushes != null)
-        {
-            int index = Brushes.IndexOf(SelectedBrush);
-            if (index != -1)
-            {
-                BrushIndex = index;
-            }
-        }
-    }
-
-    private void OnBrushIndexChanged(AvaloniaPropertyChangedEventArgs e)
-    {
-        if (suppressUpdate || (e.Sender is Visual v && !v.IsAttachedToVisualTree()))
-            return;
-
-        if (e.NewValue is int index && Brushes != null && index >= 0 && index < Brushes.Count)
-        {
-            SelectedBrush = Brushes[index];
-        }
-        else
-        {
-            SelectedBrush = null;
-        }
+        SelectedBrush = brush;
+        PopupToggle.IsChecked = false;
+        PopupToggle.Flyout.Hide();
     }
 }
