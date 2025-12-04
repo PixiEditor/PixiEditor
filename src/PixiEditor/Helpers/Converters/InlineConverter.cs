e2886c2760fad75c811790db3e5a28d02ad9b3a9
Krzysztof Krysiński <flubbet@gmail.com>
2025-12-04 15:30:56 +0100

    Updated version and brush overlay hiding

---
+++ src/PixiEditor/Helpers/Converters/InlineConverter.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace PixiEditor.Helpers.Converters;
+
+public class InlineConverter : IValueConverter
+{
+    Func<object?, bool>? simpleConvert;
+
+    public InlineConverter(Func<object, bool> simpleConvert)
+    {
+        this.simpleConvert = simpleConvert;
+    }
+
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return simpleConvert?.Invoke(value);
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
