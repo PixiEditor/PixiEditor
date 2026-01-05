8f24a90050e71f2b27ed6d6a7c11990c39931782
Krzysztof Krysiński <flubbet@gmail.com>
2026-01-05 18:24:04 +0100

    System decors init in template

--- src/PixiEditor/Views/MainWindow.axaml.cs
+++ src/PixiEditor/Views/MainWindow.axaml.cs
@@ -96,9 +96,13 @@ internal partial class MainWindow : Window
 
         PixiEditorSettings.Appearance.UseSystemDecorations.ValueChanged += (_, _) => UpdateDecorations();
 
-        UpdateDecorations();
-
         InitializeComponent();
+    }
+
+    protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
+    {
+        base.OnApplyTemplate(e);
+        UpdateDecorations();
     }
 
     private void UpdateDecorations()
