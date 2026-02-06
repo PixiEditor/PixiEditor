6c20ec0e080ea6951e83e47b4797fc8c3204a382
CPKreuz <cpkreu@gmail.com>
2026-02-06 21:39:21 +0100

    Added additional Preference and Shortcut tests to test existing files get parsed

--- tests/PixiEditor.Tests/PreferenceTests.cs
+++ tests/PixiEditor.Tests/PreferenceTests.cs
@@ -6,8 +6,17 @@ namespace PixiEditor.Tests;
 
 public class PreferenceTests
 {
+    private const string ShortcutKey = "68";
+    private const string ShortcutModifier = "0";
+    private const string ShortcutCommand = "PixiEditor.Document.ClipCanvas";
+    private const string ShortcutFile = $$$"""{"Shortcuts":[{"KeyCombination":{"Key":{{{ShortcutKey}}},"Modifiers":{{{ShortcutModifier}}}},"Commands":["{{{ShortcutCommand}}}"]}]}""";
+
+    private const string UserPreferenceStringContent = "Hello World!";
+    private const string UserPreferenceIntContent = "1";
+    private const string UserPreferenceFile = $$"""{"{{nameof(UserPreferenceStringContent)}}":"{{UserPreferenceStringContent}}","{{nameof(UserPreferenceIntContent)}}":{{UserPreferenceIntContent}}}""";
+    
     [Fact]
-    public void TestNonExistentPreferenceFileWriteEmptyJson()
+    public void TestThatNonExistentPreferenceFileWriteEmptyJson()
     {
         using var tempFile = DisposableFile.GetTemp();
 
@@ -18,7 +27,7 @@ public class PreferenceTests
     }
     
     [Fact]
-    public void TestEmptyPreferenceFileDoesNotThrow()
+    public void TestThatEmptyPreferenceFileDoesNotThrow()
     {
         using var tempFile = DisposableFile.CreateTemp();
 
@@ -29,7 +38,28 @@ public class PreferenceTests
     }
 
     [Fact]
-    public void TestEmptyShortcutFileDoesNotThrow()
+    public void TestThatExistingPreferencesGetParsed()
+    {
+        using var tempFile = DisposableFile.CreateTempWithContent(UserPreferenceFile);
+        
+        var preferences = new PreferencesSettings();
+        preferences.Init(tempFile.Path, tempFile.Path);
+
+        var localStringPreference = preferences.GetLocalPreference<string>(nameof(UserPreferenceStringContent));
+        Assert.Equal(UserPreferenceStringContent, localStringPreference);
+        
+        var roamingStringPreference = preferences.GetPreference<string>(nameof(UserPreferenceStringContent));
+        Assert.Equal(UserPreferenceStringContent, roamingStringPreference);
+
+        var localIntPreference = preferences.GetLocalPreference<int>(nameof(UserPreferenceIntContent));
+        Assert.Equal(int.Parse(UserPreferenceIntContent), localIntPreference);
+        
+        var roamingIntPreference = preferences.GetPreference<int>(nameof(UserPreferenceIntContent));
+        Assert.Equal(int.Parse(UserPreferenceIntContent), roamingIntPreference);
+    }
+
+    [Fact]
+    public void TestThatEmptyShortcutFileDoesNotThrow()
     {
         using var tempFile = DisposableFile.CreateTemp();
 
@@ -37,5 +67,25 @@ public class PreferenceTests
         var file = new ShortcutFile(tempFile.Path, controller);
 
         Assert.NotNull(file.LoadTemplate());
+    }
+
+    [Fact]
+    public void TestThatExistingShortcutFileGetsParsed()
+    {
+        using var tempFile = DisposableFile.CreateTempWithContent(ShortcutFile);
+        
+        var controller = new CommandController();
+        var file = new ShortcutFile(tempFile.Path, controller);
+
+        var loadedTemplate = file.LoadTemplate();
+        Assert.NotNull(loadedTemplate);
+        
+        Assert.Empty(loadedTemplate.Errors);
+        Assert.NotEmpty(loadedTemplate.Shortcuts);
+        
+        Assert.Equal(int.Parse(ShortcutKey), (int)loadedTemplate.Shortcuts[0].KeyCombination.Key);
+        Assert.Equal(int.Parse(ShortcutModifier), (int)loadedTemplate.Shortcuts[0].KeyCombination.Modifiers);
+        
+        Assert.Equal(ShortcutCommand, loadedTemplate.Shortcuts[0].Commands[0]);
     }
 }
