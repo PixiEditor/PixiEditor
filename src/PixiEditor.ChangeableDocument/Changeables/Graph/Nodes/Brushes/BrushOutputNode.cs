059bc049552a65a128bc58882b28eb934e44b003
Krzysztof Krysiński <flubbet@gmail.com>
2026-03-06 13:45:27 +0100

    Color space check

--- src/PixiEditor.ChangeableDocument/Changeables/Graph/Nodes/Brushes/BrushOutputNode.cs
+++ src/PixiEditor.ChangeableDocument/Changeables/Graph/Nodes/Brushes/BrushOutputNode.cs
@@ -150,8 +150,9 @@ public class BrushOutputNode : Node
         {
             if (context.RenderOutputSize.LongestAxis > 0)
             {
-                if (!CanReuseStamps.Value || ContentTexture == null || ContentTexture.Size != context.RenderOutputSize ||
-                    !drawnContentTextureOnce || Transform.Value != lastTranform)
+                if (!CanReuseStamps.Value || ContentTexture == null || ContentTexture.Size != context.RenderOutputSize
+                    || ContentTexture.ColorSpace != context.ProcessingColorSpace
+                    || !drawnContentTextureOnce || Transform.Value != lastTranform)
                 {
                     ContentTexture = cache.RequestTexture(0, context.RenderOutputSize, context.ProcessingColorSpace);
                     ContentTexture.DrawingSurface.Canvas.Save();
