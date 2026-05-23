f427a980b9bfad8f241a5b8f91e986c561b5c589
Krzysztof Krysiński <flubbet@gmail.com>
2026-05-23 14:40:59 +0200

    Changed path of autosave to be in local appdata

--- src/PixiEditor/Helpers/AutosaveUtility.cs
+++ src/PixiEditor/Helpers/AutosaveUtility.cs
@@ -4,6 +4,8 @@ namespace PixiEditor.Helpers;
 
 internal class AutosaveHelper
 {
+    static string legacyAutosavePath = Path.Join(Path.GetTempPath(), "PixiEditor", "Autosave");
+
     public static Guid? GetAutosaveGuid(string? path)
     {
         if (path is null)
@@ -15,6 +17,13 @@ internal class AutosaveHelper
 
     public static string GetAutosavePath(Guid guid)
     {
-        return Path.Join(Paths.PathToUnsavedFilesFolder, $"autosave-{guid}.pixi");
+
+        string path = Path.Join(Paths.PathToUnsavedFilesFolder, $"autosave-{guid}.pixi");
+        if (!File.Exists(path))
+        {
+            return Path.Join(legacyAutosavePath, $"autosave-{guid}.zip");
+        }
+
+        return path;
     }
 }
