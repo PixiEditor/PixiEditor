a73d7a413842858e808c84dbc8cf8648cb7f89e6
Krzysztof Krysiński <flubbet@gmail.com>
2026-05-23 15:47:33 +0200

    Safety checks in Connection duplication

--- src/PixiEditor.ChangeableDocument/Changes/NodeGraph/ConnectionsData.cs
+++ src/PixiEditor.ChangeableDocument/Changes/NodeGraph/ConnectionsData.cs
@@ -4,14 +4,15 @@ public class ConnectionsData
 {
     public Dictionary<PropertyConnection, List<PropertyConnection>> originalOutputConnections = new();
     public List<(PropertyConnection input, PropertyConnection? output)> originalInputConnections = new();
-    
-    public ConnectionsData(Dictionary<PropertyConnection, List<PropertyConnection>> originalOutputConnections, List<(PropertyConnection, PropertyConnection?)> originalInputConnections)
+
+    public ConnectionsData(Dictionary<PropertyConnection, List<PropertyConnection>> originalOutputConnections,
+        List<(PropertyConnection, PropertyConnection?)> originalInputConnections)
     {
         this.originalOutputConnections = originalOutputConnections;
         this.originalInputConnections = originalInputConnections;
     }
 
-    public ConnectionsData WithUpdatedIds(Dictionary<Guid,Guid> nodeMap)
+    public ConnectionsData WithUpdatedIds(Dictionary<Guid, Guid> nodeMap)
     {
         Dictionary<PropertyConnection, List<PropertyConnection>> newOutputConnections = new();
         foreach (var (key, value) in originalOutputConnections)
@@ -19,41 +20,57 @@ public class ConnectionsData
             Guid? sourceNodeId = key.NodeId;
             if (sourceNodeId.HasValue)
             {
-                sourceNodeId = nodeMap[sourceNodeId.Value];
+                if (!nodeMap.TryGetValue(sourceNodeId.Value, out var newSourceNodeId))
+                    continue;
+
+                sourceNodeId = newSourceNodeId;
             }
-            
+
             var valueCopy = new List<PropertyConnection>();
             foreach (var connection in value)
             {
                 Guid? targetNodeId = connection.NodeId;
                 if (targetNodeId.HasValue)
                 {
-                    targetNodeId = nodeMap[targetNodeId.Value];
+                    if (!nodeMap.TryGetValue(targetNodeId.Value, out var targetId))
+                    {
+                        continue;
+                    }
+
+                    targetNodeId = targetId;
                 }
+
                 valueCopy.Add(connection with { NodeId = targetNodeId });
             }
-            
+
             newOutputConnections.Add(key with { NodeId = sourceNodeId }, valueCopy);
         }
-        
+
         List<(PropertyConnection, PropertyConnection?)> newInputConnections = new();
         foreach (var (input, output) in originalInputConnections)
         {
             Guid? inputNodeId = input.NodeId;
             if (inputNodeId.HasValue)
             {
-                inputNodeId = nodeMap[inputNodeId.Value];
+                if (!nodeMap.TryGetValue(inputNodeId.Value, out var newInputNodeId))
+                    continue;
+
+                inputNodeId = newInputNodeId;
             }
-            
+
             Guid? outputNodeId = output?.NodeId;
             if (outputNodeId.HasValue)
             {
-                outputNodeId = nodeMap[outputNodeId.Value];
+                if (!nodeMap.TryGetValue(outputNodeId.Value, out var newOutputNodeId))
+                    continue;
+
+                outputNodeId = newOutputNodeId;
             }
-            
-            newInputConnections.Add((input with { NodeId = inputNodeId }, new PropertyConnection(outputNodeId, output?.PropertyName)));
+
+            newInputConnections.Add((input with { NodeId = inputNodeId },
+                new PropertyConnection(outputNodeId, output?.PropertyName)));
         }
-        
+
         return new ConnectionsData(newOutputConnections, newInputConnections);
     }
 }
