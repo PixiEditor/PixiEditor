3d129446450d952587fcfae4b8390495cd3800e0
Gedudo <gedudoink@pm.me>
2026-05-24 13:51:14 +0200

    Checked out the relevant files from UI-Changes Branch

--- src/PixiEditor/Views/Main/Tools/Toolbar.axaml.cs
+++ src/PixiEditor/Views/Main/Tools/Toolbar.axaml.cs
@@ -1,4 +1,6 @@
-﻿using Avalonia.Controls;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Threading;
 
 namespace PixiEditor.Views.Main.Tools;
 
@@ -8,5 +10,36 @@ public partial class Toolbar : UserControl
     {
         InitializeComponent();
     }
-}
 
+    private void ToolSetItem_OnClick(object? sender, RoutedEventArgs e)
+    {
+        Dispatcher.UIThread.Post(() => ToolSetDropdownButton.Flyout?.Hide());
+    }
+
+    private void ToolSetDropdownFlyout_OnOpened(object? sender, EventArgs e)
+    {
+        SetToolSetDropdownOpen(true);
+    }
+
+    private void ToolSetDropdownFlyout_OnClosed(object? sender, EventArgs e)
+    {
+        SetToolSetDropdownOpen(false);
+    }
+
+    private void SetToolSetDropdownOpen(bool isOpen)
+    {
+        const string openClass = "open";
+
+        if (isOpen)
+        {
+            if (!ToolSetDropdownButton.Classes.Contains(openClass))
+            {
+                ToolSetDropdownButton.Classes.Add(openClass);
+            }
+
+            return;
+        }
+
+        ToolSetDropdownButton.Classes.Remove(openClass);
+    }
+}
