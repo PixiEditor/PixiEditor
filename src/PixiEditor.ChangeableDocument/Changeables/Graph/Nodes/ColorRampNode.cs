412739a35a408a84f9c735715f4cfc2366e542f8
Ghoti Technologies <novas@novaleaf.com>
2026-05-25 10:55:09 -0700

    Beginning color ramp

---
+++ src/PixiEditor.ChangeableDocument/Changeables/Graph/Nodes/ColorRampNode.cs
@@ -0,0 +1,27 @@
+﻿using Drawie.Backend.Core;
+using Drawie.Backend.Core.ColorsImpl;
+using PixiEditor.ChangeableDocument.Rendering;
+
+namespace PixiEditor.ChangeableDocument.Changeables.Graph.Nodes;
+[NodeInfo("ColorRampNode")]
+public class ColorRampNode : Node
+{
+    public InputProperty<Texture> Fac { get; }
+    public OutputProperty<Texture> Image { get; }
+    public OutputProperty<Texture> Alpha { get; }
+    public ColorRampNode()
+    {
+        Fac = CreateInput<Texture>(nameof(Fac), "FAC", null);
+
+        Image = CreateOutput<Texture>(nameof(Image), "IMAGE", null);
+        Alpha = CreateOutput<Texture>(nameof(Alpha), "ALPHA", null);
+    }
+    protected override void OnExecute(RenderContext context)
+    {
+        
+    }
+    public override Node CreateCopy()
+    {
+        return new ColorRampNode();
+    }
+}
---
+++ src/PixiEditor/ViewModels/Document/Nodes/ColorRampNodeViewModel.cs
@@ -0,0 +1,8 @@
+﻿using PixiEditor.ChangeableDocument.Changeables.Graph.Nodes;
+using PixiEditor.UI.Common.Fonts;
+using PixiEditor.ViewModels.Nodes;
+
+namespace PixiEditor.ViewModels.Document.Nodes;
+
+[NodeViewModel("COLOR_RAMP_NODE", "IMAGE", PixiPerfectIcons.Gradient)]
+internal class ColorRampNodeViewModel : NodeViewModel<ColorRampNode>;
