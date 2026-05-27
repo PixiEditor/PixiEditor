4f726209b7727c66ee138cd574cf2162cf932bf5
Krzysztof Krysiński <flubbet@gmail.com>
2026-05-27 22:04:53 +0200

    almost working

---
+++ src/PixiEditor/ViewModels/Document/Nodes/VectorMathNodeViewModel.cs
@@ -0,0 +1,57 @@
+using PixiEditor.ChangeableDocument.Changeables.Graph.Nodes;
+using PixiEditor.ChangeableDocument.Enums;
+using PixiEditor.Extensions.Helpers;
+using PixiEditor.Models.Events;
+using PixiEditor.Models.Handlers;
+using PixiEditor.UI.Common.Fonts;
+using PixiEditor.UI.Common.Localization;
+using PixiEditor.ViewModels.Nodes;
+using PixiEditor.ViewModels.Nodes.Properties;
+
+namespace PixiEditor.ViewModels.Document.Nodes;
+
+[NodeViewModel("VECTOR_MATH_NODE", "NUMBERS", PixiPerfectIcons.PlusMinus)]
+internal class VectorMathNodeViewModel : NodeViewModel<VectorMathNode>
+{
+    private GenericEnumPropertyViewModel Mode { get; set; }
+    
+    private NodePropertyViewModel X { get; set; }
+    
+    private NodePropertyViewModel Y { get; set; }
+    
+    private NodePropertyViewModel Z { get; set; }
+    private NodePropertyViewModel S { get; set; }
+
+    public override void OnInitialized()
+    {
+        Mode = FindInputProperty("Mode") as GenericEnumPropertyViewModel;
+        X = FindInputProperty("X");
+        Y = FindInputProperty("Y");
+        Z = FindInputProperty("Z");
+        S = FindInputProperty("S");
+        
+        Mode.ValueChanged += (_, _) => ModeChanged();
+        ModeChanged();
+    }
+
+    private void ModeChanged()
+    {
+        if (Mode.Value is not VectorMathMode mode)
+            return;
+
+        DisplayName = mode.GetDescription();
+        Y.IsVisible = mode.UsesYValue();
+        Z.IsVisible = mode.UsesZValue();
+        S.IsVisible = mode.UsesSValue();
+
+        var (x, y, z) = mode.GetNaming();
+
+        x = new LocalizedString(x);
+        y = new LocalizedString(y);
+        z = new LocalizedString(z);
+
+        X.DisplayName = x;
+        Y.DisplayName = y;
+        Z.DisplayName = z;
+    }
+}
