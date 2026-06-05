1e3914e90ce5aa8ad27137d3a8f7c166ced8a505
Equbuxu <egor08281@gmail.com>
2026-06-05 17:29:08 +0300

    Add vec4d to color conversion

--- src/PixiEditor.ChangeableDocument/Changes/NodeGraph/ConversionTable.cs
+++ src/PixiEditor.ChangeableDocument/Changes/NodeGraph/ConversionTable.cs
@@ -90,9 +90,15 @@ public static class ConversionTable
                     (typeof(VecD), new TypeConverter<Color, VecD>(c => new VecD(c.R, c.G))),
                     (typeof(VecI), new TypeConverter<Color, VecI>(c => new VecI(c.R, c.G))),
                     (typeof(Vec3D), new TypeConverter<Color, Vec3D>(c => new Vec3D(c.R, c.G, c.B))),
+                    (typeof(Vec4D), new TypeConverter<Color, Vec4D>(c => c.ToVec4D())),
                     (typeof(double), new TypeConverter<Color, double>(c => c.R)),
                     (typeof(int), new TypeConverter<Color, int>(c => c.R)),
                     (typeof(float), new TypeConverter<Color, float>(c => c.R)),
+                ]
+            },
+            {
+                typeof(Vec4D), [
+                    (typeof(Paintable), new TypeConverter<Vec4D, Paintable>(c => new ColorPaintable(Color.FromVec4D(c))))
                 ]
             },
             {
