ec348db31348245b33cba76a2dd26a7a41277d7d
Ghoti-tpt <novas@novaleaf.com>
2026-06-16 14:39:45 -0700

    Remove Palette node icons
    they were placeholders before, not sure what icons would go well for them since they would be overlapping with existing icons.

--- src/PixiEditor/ViewModels/Document/Nodes/DecomposePaletteNodeViewModel.cs
+++ src/PixiEditor/ViewModels/Document/Nodes/DecomposePaletteNodeViewModel.cs
@@ -1,8 +1,7 @@
 using PixiEditor.ChangeableDocument.Changeables.Graph.Nodes;
-using PixiEditor.UI.Common.Fonts;
 using PixiEditor.ViewModels.Nodes;
 
 namespace PixiEditor.ViewModels.Document.Nodes;
 
-[NodeViewModel("DECOMPOSE_PALETTE_NODE", "COLOR", PixiPerfectIcons.CirclePile)]
+[NodeViewModel("DECOMPOSE_PALETTE_NODE", "COLOR", null)]
 internal class DecomposePaletteNodeViewModel : NodeViewModel<DecomposePaletteNode>;
--- src/PixiEditor/ViewModels/Document/Nodes/PaletteNodeViewModel.cs
+++ src/PixiEditor/ViewModels/Document/Nodes/PaletteNodeViewModel.cs
@@ -1,8 +1,7 @@
 using PixiEditor.ChangeableDocument.Changeables.Graph.Nodes;
-using PixiEditor.UI.Common.Fonts;
 using PixiEditor.ViewModels.Nodes;
 
 namespace PixiEditor.ViewModels.Document.Nodes;
 
-[NodeViewModel("PALETTE_NODE", "COLOR", PixiPerfectIcons.ColorPalette)]
+[NodeViewModel("PALETTE_NODE", "COLOR", null)]
 internal class PaletteNodeViewModel : NodeViewModel<PaletteNode>;
