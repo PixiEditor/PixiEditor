da043555007a7012a754c6c1dd00e53353b6f75f
Krzysztof Krysiński <flubbet@gmail.com>
2026-07-06 16:03:42 +0200

    More sample texture options (#1608)
    * wip more sampling options
    
    * Caching of starting textures
    
    * Fixed target sample
    
    * Disposal fixes and cleanup
    
    * Fixes
    
    * Review fixes

--- src/PixiEditor.ChangeableDocument/Changeables/Graph/Context/BrushRenderContext.cs
+++ src/PixiEditor.ChangeableDocument/Changeables/Graph/Context/BrushRenderContext.cs
@@ -12,28 +12,36 @@ namespace PixiEditor.ChangeableDocument.Changeables.Graph.Context;
 public class BrushRenderContext : RenderContext
 {
     public BrushData BrushData { get; }
-    public Texture TargetSampledTexture { get; set; }
-    public Texture TargetFullTexture { get; }
+    public Texture? LatestSampledTexture { get; set; }
+    public Texture? LatestFullTexture { get; }
+    public Texture? TargetSampleTexture { get; set; }
+    public Texture? StartingSampleTexture { get; }
+    public Texture? StartingFullTexture { get; }
     public VecD StartPoint { get; }
     public VecD LastAppliedPoint { get; }
-    public VecD TargetSampleTexturePos { get; }
+    public VecD LatestSampleTexturePos { get; }
+    public VecD StartingSampleTexturePos { get; }
     public bool DryRun { get; set; } = false;
 
-    public BrushRenderContext(Canvas? renderSurface, KeyFrameTime frameTime, ChunkResolution chunkResolution, VecI renderOutputSize, VecI documentSize, ColorSpace processingColorSpace, SamplingOptions desiredSampling, BrushData brushData, Texture? targetSampledTexture, VecD targetSampleTexturePos, Texture? targetFullTexture, IReadOnlyNodeGraph graph, VecD startPoint, VecD lastAppliedPoint, int graphCacheId, double opacity = 1) : base(renderSurface, frameTime, chunkResolution, renderOutputSize, documentSize, processingColorSpace, desiredSampling, graph, opacity)
+    public BrushRenderContext(Canvas? renderSurface, KeyFrameTime frameTime, ChunkResolution chunkResolution, VecI renderOutputSize, VecI documentSize, ColorSpace processingColorSpace, SamplingOptions desiredSampling, BrushData brushData, Texture? targetSampleTexture, Texture? latestSampledTexture, VecD latestSampleTexturePos, Texture? startingSampleTexture, VecD startingSampleTexturePos, Texture? targetStartingFullTexture, Texture? latestFullTexture, IReadOnlyNodeGraph graph, VecD startPoint, VecD lastAppliedPoint, int graphCacheId, double opacity = 1) : base(renderSurface, frameTime, chunkResolution, renderOutputSize, documentSize, processingColorSpace, desiredSampling, graph, opacity)
     {
         BrushData = brushData;
         StartPoint = startPoint;
         LastAppliedPoint = lastAppliedPoint;
-        TargetSampledTexture = targetSampledTexture;
-        TargetFullTexture = targetFullTexture;
-        TargetSampleTexturePos = targetSampleTexturePos;
+        TargetSampleTexture = targetSampleTexture;
+        LatestSampledTexture = latestSampledTexture;
+        LatestSampleTexturePos = latestSampleTexturePos;
+        LatestFullTexture = latestFullTexture;
+        StartingFullTexture = targetStartingFullTexture;
+        StartingSampleTexture = startingSampleTexture;
+        StartingSampleTexturePos = startingSampleTexturePos;
         GraphCacheId = graphCacheId;
     }
 
     public override RenderContext Clone()
     {
         return new BrushRenderContext(RenderSurface, FrameTime, ChunkResolution, RenderOutputSize, DocumentSize,
-            ProcessingColorSpace, DesiredSamplingOptions, BrushData, TargetSampledTexture, TargetSampleTexturePos, TargetFullTexture, Graph,
+            ProcessingColorSpace, DesiredSamplingOptions, BrushData, TargetSampleTexture, LatestSampledTexture, LatestSampleTexturePos, StartingSampleTexture, StartingSampleTexturePos, StartingFullTexture, LatestFullTexture, Graph,
             StartPoint, LastAppliedPoint, GraphCacheId, Opacity)
         {
             VisibleDocumentRegion = VisibleDocumentRegion,
