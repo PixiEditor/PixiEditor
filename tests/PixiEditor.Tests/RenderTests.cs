a9fad180c90eccee79d01947b73d66936872cbd7
Krzysztof Krysiński <flubbet@gmail.com>
2026-09-01 10:23:32 +0200

    Added test files

--- tests/PixiEditor.Tests/RenderTests.cs
+++ tests/PixiEditor.Tests/RenderTests.cs
@@ -56,6 +56,12 @@ public class RenderTests : FullPixiEditorTest
     [InlineData("GradientArrays")]
     [InlineData("SeparateAndCombineColor")]
     [InlineData("CombineSeparateColorContextful")]
+    [InlineData("CombineSeparateRgb")]
+    [InlineData("CombineSeparateHsl")]
+    [InlineData("CombineSeparateHsv")]
+    [InlineData("CombineSeparateRgbNormalized")]
+    [InlineData("CombineSeparateHslNormalized")]
+    [InlineData("CombineSeparateHsvNormalized")]
     public void TestThatPixiFilesRenderTheSameResultAsSavedPng(string fileName, string? resultName = null)
     {
         if (!DrawingBackendApi.Current.IsHardwareAccelerated)
