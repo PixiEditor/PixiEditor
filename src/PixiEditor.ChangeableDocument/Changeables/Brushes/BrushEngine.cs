d77fea4d55d666c8af51a8c35bff0c1a33eb1d22
Krzysztof Krysiński <flubbet@gmail.com>
2026-09-14 15:08:02 +0200

    Fixed Brush Engine sampling

--- src/PixiEditor.ChangeableDocument/Changeables/Brushes/BrushEngine.cs
+++ src/PixiEditor.ChangeableDocument/Changeables/Brushes/BrushEngine.cs
@@ -500,7 +500,7 @@ public class BrushEngine : IDisposable
             if (shape.Bounds is { Width: > 0, Height: > 0 })
             {
                 //context.TargetSampledTexture?.Dispose();
-                RectI size = (RectI)brushShapeCache.TightBounds.Round().Inflate(brushNode.TargetOversample.Value);
+                RectI size = (RectI)rect.Round().Inflate(brushNode.TargetOversample.Value);
                 targetSampleUnderRect = UpdateSurfaceUnderRect(TargetStampCacheId, target,
                     size, colorSpace,
                     brushNode.AllowSampleStacking.Value);
@@ -515,7 +515,7 @@ public class BrushEngine : IDisposable
                 }
 
                 context.LatestSampledTexture = latestSampleUnderRect;
-                context.RenderOutputSize = ((RectI)brushShapeCache.TightBounds.Round()).Size;
+                context.RenderOutputSize = ((RectI)rect.Round()).Size;
                 context.GraphCacheId = nextRenderId + 1;
             }
         }
